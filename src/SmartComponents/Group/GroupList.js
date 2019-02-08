a611253044b6358f93d1a431e5b72a6ac3780d79
Laura Galis <lgalis@redhat.com>
2019-02-08 14:19:22 -0500

    Change GroupList to component

--- src/SmartComponents/Group/GroupList.js
+++ src/SmartComponents/Group/GroupList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 import propTypes from 'prop-types';
 import { PageHeader, PageHeaderTitle, Section } from '@red-hat-insights/insights-frontend-components';
 import { fetchUsersByGroupId } from '../../Helpers/Group/GroupHelper';
@@ -7,74 +7,104 @@ import {
   DataList,
   DataListItem,
   DataListCell,
+  DataListCheck,
+  DataListAction,
   DataListToggle,
   DataListContent,
   Text,
   TextVariants,
   TextContent } from '@patternfly/react-core';
 
+class GroupList extends Component {
 
-const GroupList = ({ isLoading, items, noItems = 'No Items' }) => {
-
-  if (isLoading)
-  {
-    return (
-      <PageHeader>
-        <PageHeaderTitle title={ noItems }/>
-      </PageHeader>
-    );
-  }
-
-  let expandedList = [];
-
-  const toggle = id => {
-    const expanded = expandedList;
-    const index = expanded.indexOf(id);
-    const newExpanded =
-      index >= 0 ? [ ...expanded.slice(0, index), ...expanded.slice(index + 1, expanded.length) ] : [ ...expanded, id ];
-    expandedList = newExpanded;
+  state= {
+    expanded: []
   };
 
-  const fetchUserListForGroup = (group) => {
+  toggle = id => {
+    const expanded = this.state.expanded;
+    const index = expanded.indexOf(id);
+    const newExpanded =
+        index >= 0 ? [ ...expanded.slice(0, index), ...expanded.slice(index + 1, expanded.length) ] : [ ...expanded, id ];
+    this.setState(() => ({ expanded: newExpanded }));
+  };
+
+  fetchUserListForGroup = (group) => {
     if (!group.members) {
       return '';
     }
+
     return group.members.map(user => `${user.first_name} ${user.last_name}`).join(', ');
   };
 
-  // <GroupDetail isExpanded={ expandedList.includes(item.name) } toggle={ toggle }/>) }
-  return (
-    <React.Fragment>
-      <Bullseye>
-        <div>
-          { isLoading && (<span color={ '#00b9e4' }> Loading...</span>) }
-        </div>
-      </Bullseye>
-      <Section type='content'>
-        { (items && items.length > 0) && (
-          <DataList aria-label="Expandable data list">
-            { items.map((item) => {
-              return (
-                <DataListItem key= { item.id } aria-labelledby="simple-item1">
-                  <DataListCell>
-                    <span id={ item.id }>{ item.name } </span>
-                  </DataListCell>
-                  <DataListCell>
-                    { fetchUserListForGroup(item) }
-                  </DataListCell>
-                </DataListItem>); }
-            )
-            }
-          </DataList>)
-        }
-      </Section>
-    </React.Fragment>
-  );
-};
+  render() {
+    if (this.props.isLoading) {
+      return (
+        <PageHeader>
+          <PageHeaderTitle title={ this.props.noItems }/>
+        </PageHeader>
+      );
+    }
+
+    // <GroupDetail isExpanded={ expandedList.includes(item.name) } toggle={ toggle }/>) }
+    return (
+      <React.Fragment>
+        <Bullseye>
+          <div>
+            { this.props.isLoading && (<span color={ '#00b9e4' }> Loading...</span>) }
+          </div>
+        </Bullseye>
+        <Section type='content'>
+          { (this.props.items && this.props.items.length > 0) && (
+            <DataList aria-label="Expandable data list">
+              { this.props.items.map((item) => {
+                return (
+                  <DataListItem key={ `toggle-${item.id}` }
+                    aria-labelledby={ `check-group-${item.id}` }
+                    isExpanded={ this.state.expanded.includes(`toggle-${item.id}`) }>
+                    <DataListToggle
+                      onClick={ () => this.toggle(`toggle-${item.id}`) }
+                      isExpanded={ this.state.expanded.includes(`toggle-${item.id}`) }
+                      id={ `toggle-${item.id}` }
+                      aria-labelledby={ `toggle-${item.id} group-${item.id}` }
+                      aria-label="Toggle details for"
+                    />
+                    <DataListCheck aria-labelledby={ `check-group-${item.id}` } name={ `check-group-${item.id}` }/>
+                    <DataListCell>
+                      <span id={ item.id }>{ item.name } </span>
+                    </DataListCell>
+                    <DataListCell>
+                      { this.fetchUserListForGroup(item) }
+                    </DataListCell>
+                    <DataListAction
+                      aria-labelledby={ `check-group-${item.id} check-action-action${item.id}` }
+                      id={ `check-action-action${item.id}` }
+                      aria-label="Actions"
+                    />
+                    <DataListContent aria-label="Group Content Details"
+                      isHidden={ !this.state.expanded.includes(`toggle-${item.id}`) }>
+                      <p>
+                          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                          labore et
+                          dolore magna aliqua.
+                      </p>
+                    </DataListContent>
+                  </DataListItem>);
+              }
+              )
+              }
+            </DataList>)
+          }
+        </Section>
+      </React.Fragment>
+    );
+  };
+}
 
 GroupList.propTypes = {
   isLoading: propTypes.bool,
   items: propTypes.array,
   noItems: propTypes.string
 };
+
 export default GroupList;
