3684a5cfdc5ac21ce24e1a72a1ca6a32a3c7ae7a
Laura Galis <lgalis@redhat.com>
2019-02-24 18:37:39 -0500

    Update User and group details

--- src/Helpers/Shared/userLogin.js
+++ src/Helpers/Shared/userLogin.js
@@ -10,3 +10,17 @@ let approvalApi = new ApprovalApi.AdminsApi();
 export function getApprovalApi() {
   return approvalApi;
 }
+
+let RoleBasedAccessControl = require('role_based_access_control');
+
+let defaultRbacClient = RoleBasedAccessControl.ApiClient.instance;
+// Configure HTTP basic authorization: basic_auth
+let rbac_basic_auth = defaultRbacClient.authentications['basic_auth'];
+rbac_basic_auth.username = 'lgalis@redhat.com';
+rbac_basic_auth.password = 'redhat';
+
+let rbacApi = new RoleBasedAccessControl.AccessApi();
+
+export function getRbacApi() {
+  return rbacApi;
+}
