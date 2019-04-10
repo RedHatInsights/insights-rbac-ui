cdb2c19a98d59c1b1b8d7bbcefeeb450505f2a04
Martin Marosi <marvusm.mmi@gmail.com>
2019-04-10 15:36:08 +0200

    Fixed tab navigation.

--- src/SmartComponents/AppTabs/AppTabs.js
+++ src/SmartComponents/AppTabs/AppTabs.js
@@ -1,40 +1,38 @@
-import React, { Component } from 'react';
-import propTypes from 'prop-types';
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
 import { Tabs, Tab } from '@patternfly/react-core';
 import './apptabs.scss';
 
 const tabItems = [{ eventKey: 0, title: 'Principals', name: '/users' }, { eventKey: 1, title: 'Groups', name: '/groups' }];
 
-class AppTabs extends Component {
-  state = {
-    activeTabKey: 0
-  };
+const AppTabs = ({ children, history: { push }, location: { pathname }}) => {
+  const activeTab = tabItems.find(({ name }) => pathname.includes(name));
+  const handleTabClick = (_event, tabIndex) => push(tabItems[tabIndex].name);
 
-  // Toggle currently active tab
-  handleTabClick = (event, tabIndex) => {
-    this.setState({
-      activeTabKey: tabIndex
-    });
-
-    this.props.children.props.childProps.history.push(tabItems[tabIndex].name);
-  };
-
-  render() {
-    return (
-      <React.Fragment>
-        <Tabs className="ins-tabs" activeKey={ this.state.activeTabKey } onSelect={ this.handleTabClick }>
+  return (
+    <Fragment>
+      <div className="ins-tabs">
+        <Tabs activeKey={ activeTab ? activeTab.eventKey : 0 } onSelect={ handleTabClick }>
           { tabItems.map((item) => <Tab title={ item.title } key={ item.eventKey } eventKey={ item.eventKey } name={ item.name }/>) }
         </Tabs>
-        { this.props.children }
-      </React.Fragment>
-    );
-  }
-}
-
-AppTabs.propTypes = {
-  children: propTypes.any,
-  location: propTypes.object,
-  history: propTypes.object
+      </div>
+      { children }
+    </Fragment>
+  );
 };
 
-export default AppTabs;
+AppTabs.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.node,
+    PropTypes.arrayOf(PropTypes.node)
+  ]),
+  location: PropTypes.shape({
+    pathname: PropTypes.string.isRequired
+  }),
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired
+  })
+};
+
+export default withRouter(AppTabs);
