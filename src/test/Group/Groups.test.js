f9cbe4808ac7dded7268dee222c772ab04cd9b46
Laura Galis <lgalis@redhat.com>
2019-06-12 08:40:23 -0400

    Added group tests and updated snapshots

---
+++ src/test/Group/Groups.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import thunk from 'redux-thunk';
+import { shallow } from 'enzyme';
+import configureStore from 'redux-mock-store' ;
+import { shallowToJson } from 'enzyme-to-json';
+import promiseMiddleware from 'redux-promise-middleware';
+import Groups from '../../SmartComponents/Group/Groups';
+import { notificationsMiddleware } from '@redhat-cloud-services/frontend-components-notifications';
+import { groupsInitialState } from '../../redux/reducers/groupReducer';
+
+describe('<Groups />', () => {
+
+  let initialProps;
+  const middlewares = [ thunk, promiseMiddleware(), notificationsMiddleware() ];
+  let mockStore;
+  let initialState;
+
+  beforeEach(() => {
+    initialProps = {};
+    mockStore = configureStore(middlewares);
+    initialState = { groupReducer: { ...groupsInitialState, isLoading: false }, userReducer: { ...groupsInitialState }};
+  });
+
+  it('should render correctly', () => {
+    const store = mockStore(initialState);
+    const wrapper = shallow(<Groups store={ store } { ...initialProps } />);
+    expect(shallowToJson(wrapper)).toMatchSnapshot();
+  });
+
+  it('should render correctly in loading state', () => {
+    const store = mockStore(initialState);
+    const wrapper = shallow(<Groups store={ store } { ...initialProps } isLoading />);
+    expect(shallowToJson(wrapper)).toMatchSnapshot();
+  });
+});
---
+++ src/test/Group/add-group-modal.test.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { shallow, mount } from 'enzyme';
+import configureStore from 'redux-mock-store' ;
+import { shallowToJson } from 'enzyme-to-json';
+import { Button } from '@patternfly/react-core';
+import { MemoryRouter, Route } from 'react-router-dom';
+import promiseMiddleware from 'redux-promise-middleware';
+import { notificationsMiddleware } from '@redhat-cloud-services/frontend-components-notifications/';
+import { RBAC_API_BASE } from '../../Utilities/Constants';
+import AddGroupModal from '../../SmartComponents/Group/add-group-modal';
+import { groupsInitialState } from '../../redux/reducers/groupReducer';
+import { componentTypes } from '@data-driven-forms/react-form-renderer/dist/index';
+
+describe('<AddGroupModal />', () => {
+  let initialProps;
+  const middlewares = [ thunk, promiseMiddleware(), notificationsMiddleware() ];
+  let mockStore;
+  let initialState;
+
+  const GroupWrapper = ({ store, children, initialEntries = []}) => (
+    <Provider store={ store }>
+      <MemoryRouter initialEntries={ initialEntries }>
+        { children }
+      </MemoryRouter>
+    </Provider>
+  );
+
+  beforeEach(() => {
+    initialProps = {
+      closeUrl: '/groups',
+      groupData: {
+        name: 'Foo',
+        id: '1'
+      }
+    };
+    mockStore = configureStore(middlewares);
+    initialState = {
+      groupReducer: {
+        ...groupsInitialState,
+        isLoading: true,
+        groupData: {
+          name: 'Foo',
+          id: '1'
+        }
+      }
+    };
+  });
+
+  it('should render correctly', () => {
+    const wrapper = shallow(<AddGroupModal { ...initialProps } />);
+    expect(shallowToJson(wrapper)).toMatchSnapshot();
+  });
+
+  it('should redirect back to close URL', (done) => {
+    const store = mockStore(initialState);
+
+    apiClientMock.get(`${RBAC_API_BASE}/groups/action-modal/1`, mockOnce(
+      { body: {
+        component: componentTypes.TEXTAREA_FIELD,
+        name: 'comments',
+        type: 'text',
+        isRequired: false,
+        label: 'Comment'
+      }
+      }));
+
+    const wrapper = mount(
+      <GroupWrapper store={ store } initialEntries={ [ '/foo/url' ] }>
+        <Route to="/foo/url" render={ args => <AddGroupModal { ...initialProps } { ...args } /> }  />
+      </GroupWrapper>
+    );
+
+    setImmediate(() => {
+      wrapper.find(Button).first().simulate('click');
+      expect(wrapper.find(MemoryRouter).instance().history.location.pathname).toEqual('/groups');
+      done();
+    });
+  });
+});
+
