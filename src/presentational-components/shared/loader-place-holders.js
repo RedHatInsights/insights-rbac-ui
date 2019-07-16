a30f70af34dd937f7e4f0c40deeb17dfe99ccb4b
Laura Galis <lgalis@redhat.com>
2019-07-16 15:42:30 -0400

    Update tests and filter

--- src/presentational-components/shared/loader-place-holders.js
+++ src/presentational-components/shared/loader-place-holders.js
@@ -1,37 +1,50 @@
-import React from 'react';
-import ContentLoader from 'react-content-loader';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
-import { Card, CardBody, Grid, GridItem } from '@patternfly/react-core';
+import {
+  DataList,
+  DataListCell,
+  DataListItem,
+  DataListItemRow,
+  DataListItemCells
+} from '@patternfly/react-core';
+import { Section } from '@redhat-cloud-services/frontend-components';
+import ContentLoader from 'react-content-loader';
 
-export const CardLoader = ({ items, ...props }) => (
-  <Grid  gutter="md">
-    <GridItem sm={ 12 } style={ { padding: 24 } }>
-      <Grid  gutter="md">
-        { [ ...Array(items) ].map((_item, index) => <GridItem sm={ 12 } md={ 6 } lg={ 3 } key={ index }><Card>
-          <CardBody>
-            <ContentLoader
-              height={ 160 }
-              width={ 300 }
-              speed={ 2 }
-              primaryColor="#f3f3f3"
-              secondaryColor="#ecebeb"
-              { ...props }
-            >
-              <rect x="2" y="99" rx="3" ry="3" width="300" height="6.4" />
-            </ContentLoader>
-          </CardBody>
-        </Card></GridItem>) }
-      </Grid>
-    </GridItem>
-  </Grid>
+export const ListLoader = ({ items, ...props }) => (
+  <Fragment>
+    <Section className="data-table-pane">
+      <DataList aria-labelledby="datalist-placeholder" style={ { margin: 32 } }>
+        { [ ...Array(items) ].map((_item, index) => (
+          <DataListItem key={ index } aria-labelledby="datalist-item-placeholder">
+            <DataListItemRow>
+              <DataListItemCells dataListCells={ [
+                <DataListCell key="1">
+                  <ContentLoader
+                    height={ 12 }
+                    width={ 300 }
+                    speed={ 2 }
+                    primaryColor="#FFFFFF"
+                    secondaryColor="#ecebeb"
+                    { ...props }>
+                    <rect x="0" y="0" rx="0" ry="0" width="300" height="12" />
+                  </ContentLoader>
+                </DataListCell>
+              ] }
+              />
+            </DataListItemRow>
+          </DataListItem>
+        )) }
+      </DataList>
+    </Section>
+  </Fragment>
 );
 
-CardLoader.propTypes = {
+ListLoader.propTypes = {
   items: PropTypes.number
 };
 
-CardLoader.defaultProps = {
-  items: 1
+ListLoader.defaultProps = {
+  items: 5
 };
 
 export const AppPlaceholder = props => (
@@ -45,6 +58,6 @@ export const AppPlaceholder = props => (
       { ...props }>
       <rect x="0" y="0" rx="0" ry="0" width="420" height="10" />
     </ContentLoader>
-    <CardLoader />
+    <ListLoader />
   </div>
 );
