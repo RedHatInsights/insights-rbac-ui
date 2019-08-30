11b88ce8fb85749ddc50f9cd4f299c5fb5696917
Laura Galis <lgalis@redhat.com>
2019-08-30 14:22:43 -0400

    Add toolbar button for Add / remove members to/from a group in the group details page

---
+++ src/smart-components/group/principal/edit-group-members.js
@@ -0,0 +1,185 @@
+import React, { useEffect, useState } from 'react';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import PropTypes from 'prop-types';
+import CreatableSelect from 'react-select/creatable';
+import { ActionGroup,
+  Button,
+  Modal,
+  Split,
+  SplitItem,
+  Stack,
+  StackItem,
+  TextContent,
+  Text,
+  TextVariants } from '@patternfly/react-core';
+import { addNotification } from '@redhat-cloud-services/frontend-components-notifications/';
+import { addGroup, fetchGroups, fetchGroup, updateGroup } from '../../../redux/actions/group-actions';
+
+const components = {
+  DropdownIndicator: null
+};
+
+const AddGroupMembers = ({
+  history: { push },
+  match: { params: { id }},
+  addNotification,
+  closeUrl,
+  updateGroup
+}) => {
+  const [ inputValue, setInputValue ] = useState('');
+  const [ selectedUsers, setSelectedUsers ] = useState([]);
+  const [ optionIdx, setOptionIdx ] = useState(0);
+
+  const createOption = (label) => {
+    let idx = optionIdx;
+    setOptionIdx(optionIdx + 1);
+    return {
+      label,
+      value: `${label}_${idx}`
+    };
+  };
+
+  const setGroupData = (groupData) => {
+    if (groupData) {
+      setSelectedUsers(groupData.principals.map(user => (createOption(user.username))));
+    }
+  };
+
+  const fetchData = () => {
+    fetchGroup(id).payload.then((data) => setGroupData(data)).catch(() => setGroupData(undefined));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const onSubmit = data => {
+    const user_data = { ...data, user_list: selectedUsers.map(user => ({ username: user.label })) };
+    updateGroup(user_data).then(() => fetchGroups()).then(push('/groups'));
+  };
+
+  const onCancel = () => {
+    addNotification({
+      variant: 'warning',
+      title: 'Adding members to group',
+      description: 'Adding members to group was cancelled by the user.'
+    });
+    push(closeUrl);
+  };
+
+  const handleChange = (value) => {
+    setSelectedUsers(value);
+  };
+
+  const handleInputChange = (val) => {
+    setInputValue(val);
+  };
+
+  const handleKeyDown = (event) => {
+    if (!inputValue) {return;}
+
+    switch (event.key) {
+      case 'Enter':
+      case 'Tab':
+        if (selectedUsers) {
+          if (!selectedUsers.find(user => (user.label === inputValue))) {
+            setSelectedUsers([ ...selectedUsers, createOption(inputValue) ]);
+          }
+        }
+        else {
+          setSelectedUsers([ createOption(inputValue) ]);
+        }
+
+        setInputValue('');
+        event.preventDefault();
+    }
+  };
+
+  return (
+    <Modal
+      title={ 'Add group members' }
+      width={ '40%' }
+      isOpen
+      onClose={ onCancel }>
+      <Stack gutter="md">
+        <StackItem>
+          <TextContent>
+            <Text component={ TextVariants.h6 }>Select Members for this group.</Text>
+          </TextContent>
+          <CreatableSelect
+            components={ components }
+            inputValue={ inputValue }
+            defaultValue={ selectedUsers }
+            isClearable
+            isMulti
+            menuIsOpen={ false }
+            onChange={ handleChange }
+            onInputChange={ handleInputChange }
+            onKeyDown={ handleKeyDown }
+            placeholder="Type the exact user name and press enter..."
+            value={ selectedUsers }
+          />
+        </StackItem>
+        <StackItem>
+          <ActionGroup>
+            <Split gutter="md">
+              <SplitItem>
+                <Button aria-label={ 'Save' }
+                  variant="primary"
+                  type="submit"
+                  onClick={ onSubmit }>Save</Button>
+              </SplitItem>
+              <SplitItem>
+                <Button  aria-label='Cancel'
+                  variant='secondary'
+                  type='button'
+                  onClick={ onCancel }>Cancel</Button>
+              </SplitItem>
+            </Split>
+          </ActionGroup>
+        </StackItem>
+      </Stack>
+    </Modal>
+  );
+};
+
+AddGroupMembers.defaultProps = {
+  users: [],
+  inputValue: '',
+  selectedGroup: undefined,
+  closeUrl: '/groups',
+  selectedUsers: []
+};
+
+AddGroupMembers.propTypes = {
+  history: PropTypes.shape({
+    goBack: PropTypes.func.isRequired
+  }).isRequired,
+  addGroup: PropTypes.func.isRequired,
+  addNotification: PropTypes.func.isRequired,
+  fetchGroups: PropTypes.func.isRequired,
+  fetchGroup: PropTypes.func.isRequired,
+  selectedGroup: PropTypes.object,
+  inputValue: PropTypes.string,
+  users: PropTypes.array,
+  selectedUsers: PropTypes.array,
+  match: PropTypes.object,
+  closeUrl: PropTypes.string,
+  updateGroup: PropTypes.func.isRequired
+};
+
+const mapStateToProps = ({ groupReducer: { isLoading }}) => ({
+  isLoading
+});
+
+const mapDispatchToProps = (dispatch) => bindActionCreators({
+  addNotification,
+  addGroup,
+  updateGroup,
+  fetchGroup,
+  fetchGroups
+}, dispatch);
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(AddGroupMembers));
--- src/smart-components/group/principal/principals.js
+++ src/smart-components/group/principal/principals.js
@@ -1,12 +1,15 @@
 import React, { Fragment, useState } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { Link, Route } from 'react-router-dom';
 import { expandable } from '@patternfly/react-table';
 import { TableToolbarView } from '../../../presentational-components/shared/table-toolbar-view';
 import { createRows } from './principal-table-helpers';
 import { fetchGroup } from '../../../redux/actions/group-actions';
 import { ListLoader } from '../../../presentational-components/shared/loader-placeholders';
 import { defaultSettings } from '../../../helpers/shared/pagination';
+import { Button, ToolbarGroup, ToolbarItem } from '@patternfly/react-core';
+import AddGroupMembers from './edit-group-members';
 
 const columns = [{ title: 'Name', cellFormatters: [ expandable ]}, 'Email', 'First name', 'Last name' ];
 
@@ -20,6 +23,10 @@ const GroupPrincipals = ({ uuid, fetchGroup, history, principals, pagination }) 
       });
     }
   };
+
+  const routes = () => <Fragment>
+    <Route exact path={ `/groups/detail/${uuid}/add_members` } component={ AddGroupMembers } />
+  </Fragment>;
 
   const actionResolver = (_principalData, { rowIndex }) =>
     rowIndex % 2 === 1 ? null :
@@ -37,6 +44,19 @@ const GroupPrincipals = ({ uuid, fetchGroup, history, principals, pagination }) 
         }
       ];
 
+  const toolbarButtons = () => <ToolbarGroup>
+    <ToolbarItem>
+      <Link to={ `/groups/detail/${uuid}/add_members` }>
+        <Button
+          variant="primary"
+          aria-label="Add member"
+        >
+          Add member
+        </Button>
+      </Link>
+    </ToolbarItem>
+  </ToolbarGroup>;
+
   return (
     <Fragment>
       { !uuid && <ListLoader/> }
@@ -47,12 +67,14 @@ const GroupPrincipals = ({ uuid, fetchGroup, history, principals, pagination }) 
         columns={ columns }
         fetchData={ fetchData }
         request={ fetchGroup }
+        routes={ routes }
         actionResolver={ actionResolver }
         titlePlural="principals"
         titleSingular="principal"
         pagination={ pagination }
         filterValue={ filterValue }
         setFilterValue={ setFilterValue }
+        toolbarButtons = { toolbarButtons }
       /> }
     </Fragment>);
 };
@@ -73,11 +95,15 @@ GroupPrincipals.propTypes = {
   history: PropTypes.shape({
     push: PropTypes.func.isRequired
   }),
+  location: PropTypes.shape({
+    pathname: PropTypes.string
+  }),
   principals: PropTypes.array,
   platforms: PropTypes.array,
   isLoading: PropTypes.bool,
   fetchGroup: PropTypes.func.isRequired,
   uuid: PropTypes.string,
+  match: PropTypes.object,
   pagination: PropTypes.shape({
     limit: PropTypes.number.isRequired,
     offset: PropTypes.number.isRequired,
