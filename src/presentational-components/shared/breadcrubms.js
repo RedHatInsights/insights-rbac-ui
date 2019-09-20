fb6372e542e81a2701ef3e90bea6f832b89618c4
Laura Galis <lgalis@redhat.com>
2019-09-20 08:19:38 -0400

    Remove extraneous 'Fragment'

--- src/presentational-components/shared/breadcrubms.js
+++ src/presentational-components/shared/breadcrubms.js
@@ -1,19 +1,17 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
 import { Breadcrumb, BreadcrumbItem } from '@patternfly/react-core';
 
 const RbacBreadcrumbs = (breadcrumbs) => {
   return (
-    <Fragment>
-      { breadcrumbs && <Breadcrumb className="pf-u-pt-xl">
-        { Object.values(breadcrumbs).map(item => (
-          <BreadcrumbItem key={ item.title } isActive={ item.isActive }>
-            { (item.to && <NavLink exact to={ item.to }>{ item.title }</NavLink>) || item.title }
-          </BreadcrumbItem>
-        )) }
-      </Breadcrumb> }
-    </Fragment>
+    breadcrumbs ? <Breadcrumb className="pf-u-pt-xl">
+      { Object.values(breadcrumbs).map(item => (
+        <BreadcrumbItem key={ item.title } isActive={ item.isActive }>
+          { (item.to && <NavLink exact to={ item.to }>{ item.title }</NavLink>) || item.title }
+        </BreadcrumbItem>
+      )) }
+    </Breadcrumb> : null
   );
 };
 
