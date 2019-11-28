921bf36a4d3d01e9525845c2529e5e034b665249
PanSpagetka <rknaur@redhat.com>
2019-11-28 11:05:01 +0100

    Add test for Group roles

---
+++ src/test/smart-components/group/role/group-roles.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import thunk from 'redux-thunk';
+import { shallow } from 'enzyme';
+import configureStore from 'redux-mock-store';
+import { shallowToJson } from 'enzyme-to-json';
+import promiseMiddleware from 'redux-promise-middleware';
+import GroupRoles from '../../../../smart-components/group/role/group-roles';
+import { notificationsMiddleware } from '@redhat-cloud-services/frontend-components-notifications';
+import { rolesInitialState } from '../../../../redux/reducers/role-reducer';
+
+describe('<GroupPrincipals />', () => {
+
+  let initialProps;
+  const middlewares = [ thunk, promiseMiddleware(), notificationsMiddleware() ];
+  let mockStore;
+  let initialState;
+
+  beforeEach(() => {
+    initialProps = {};
+    mockStore = configureStore(middlewares);
+    initialState = { roleReducer: { ...rolesInitialState, isLoading: false }};
+  });
+
+  it('should render correctly', () => {
+    const store = mockStore(initialState);
+    const wrapper = shallow(<GroupRoles store={ store } { ...initialProps } />);
+    expect(shallowToJson(wrapper)).toMatchSnapshot();
+  });
+
+  it('should render correctly in loading state', () => {
+    const store = mockStore(initialState);
+    const wrapper = shallow(<GroupRoles store={ store } { ...initialProps } isLoading />);
+    expect(shallowToJson(wrapper)).toMatchSnapshot();
+  });
+});
