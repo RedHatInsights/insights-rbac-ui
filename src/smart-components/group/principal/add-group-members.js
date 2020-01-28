97f1041d37c1e9d35d0ef961ae2d5ee87cbe3953
Karel Hala <khala@redhat.com>
2020-01-28 17:33:08 +0100

    Use user table to add members to group

--- src/smart-components/group/principal/add-group-members.js
+++ src/smart-components/group/principal/add-group-members.js
@@ -3,140 +3,61 @@ import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import PropTypes from 'prop-types';
-import CreatableSelect from 'react-select/creatable';
-import { ActionGroup,
-  Button,
-  Modal,
-  Split,
-  SplitItem,
-  Stack,
-  StackItem,
-  TextContent,
-  Text,
-  TextVariants } from '@patternfly/react-core';
+import { Button, Modal } from '@patternfly/react-core';
 import { addNotification } from '@redhat-cloud-services/frontend-components-notifications/';
 import { addGroup, fetchGroup, addMembersToGroup } from '../../../redux/actions/group-actions';
-
-const components = {
-  DropdownIndicator: null
-};
+import UsersList from '../add-group/users-list';
 
 const AddGroupMembers = ({
   history: { push },
   match: { params: { uuid }},
   addNotification,
-  fetchData,
   closeUrl,
-  addMembersToGroup
+  addMembersToGroup,
+  fetchGroup
 }) => {
-  const [ inputValue, setInputValue ] = useState('');
   const [ selectedUsers, setSelectedUsers ] = useState([]);
-  const [ optionIdx, setOptionIdx ] = useState(0);
-
-  const createOption = (label) => {
-    let idx = optionIdx;
-    setOptionIdx(optionIdx + 1);
-    return {
-      label,
-      value: `${label}_${idx}`
-    };
-  };
 
   const onSubmit = () => {
-    const user_list = selectedUsers.map(user => ({ username: user.label }));
-    return addMembersToGroup(uuid, user_list).then(() => {
-      fetchData();
-      push(closeUrl);
-    });
+    const userList = selectedUsers.map(user => ({ username: user.label }));
+    if (userList.length > 0) {
+      addNotification({
+        variant: 'info',
+        title: `Adding member${userList.length > 1 ? 's' : ''} to group`,
+        description: `Adding member${userList.length > 1 ? 's' : ''} to group initiated.`
+      });
+      addMembersToGroup(uuid, userList).then(() => fetchGroup(uuid));
+    }
+
+    push(closeUrl);
   };
 
   const onCancel = () => {
     addNotification({
       variant: 'warning',
-      title: 'Adding members to group',
+      title: `Adding member${selectedUsers.length > 1 ? 's' : ''} to group`,
       dismissable: true,
-      description: 'Adding members to group was cancelled by the user.'
+      description: `Adding member${selectedUsers.length > 1 ? 's' : ''} to group was cancelled by the user.`
     });
     push(closeUrl);
   };
 
-  const handleChange = (value) => {
-    setSelectedUsers(value);
-  };
-
-  const handleInputChange = (val) => {
-    setInputValue(val);
-  };
-
-  const handleKeyDown = (event) => {
-    if (!inputValue) {return;}
-
-    switch (event.key) {
-      case 'Enter':
-      case 'Tab':
-        if (selectedUsers) {
-          if (!selectedUsers.find(user => (user.label === inputValue))) {
-            setSelectedUsers([ ...selectedUsers, createOption(inputValue) ]);
-          }
-        }
-        else {
-          setSelectedUsers([ createOption(inputValue) ]);
-        }
-
-        setInputValue('');
-        event.preventDefault();
-    }
-  };
-
   return (
     <Modal
-      title={ 'Add group members' }
+      title="Add members to the group"
       width={ '40%' }
       isOpen
+      isFooterLeftAligned
+      actions={ [
+        <Button key="confirm" isDisabled={ selectedUsers.length === 0 } variant="primary" onClick={ onSubmit }>
+          Add to group
+        </Button>,
+        <Button key="cancel" variant="link" onClick={ onCancel }>
+          Cancel
+        </Button>
+      ] }
       onClose={ onCancel }>
-      <Stack gutter="md">
-        <StackItem>
-          <TextContent>
-            <Text component={ TextVariants.h6 }>Select Members for this group.</Text>
-          </TextContent>
-          <CreatableSelect
-            components={ components }
-            inputValue={ inputValue }
-            defaultValue={ selectedUsers }
-            isClearable
-            isMulti
-            menuIsOpen={ false }
-            onChange={ handleChange }
-            onInputChange={ handleInputChange }
-            onKeyDown={ handleKeyDown }
-            placeholder="Type the exact user name and press enter..."
-            value={ selectedUsers }
-          />
-        </StackItem>
-        <StackItem>
-          <ActionGroup>
-            <Split gutter="md">
-              <SplitItem>
-                <Button
-                  aria-label="Save"
-                  variant="primary"
-                  type="submit"
-                  onClick={ onSubmit }
-                >
-                  Save
-                </Button>
-              </SplitItem>
-              <SplitItem>
-                <Button
-                  aria-label='Cancel'
-                  variant='secondary'
-                  type='button'
-                  onClick={ onCancel }>Cancel</Button>
-              </SplitItem>
-            </Split>
-          </ActionGroup>
-        </StackItem>
-      </Stack>
+      <UsersList selectedUsers={ selectedUsers } setSelectedUsers={ setSelectedUsers } />
     </Modal>
   );
 };
