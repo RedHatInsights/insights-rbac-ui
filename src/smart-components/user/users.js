a3ee841f363e1e2fbee07f7086ab1fe207ceb3ec
PanSpagetka <rknaur@redhat.com>
2020-02-06 14:28:19 +0100

    Add link to user management list

--- src/smart-components/user/users.js
+++ src/smart-components/user/users.js
@@ -1,11 +1,22 @@
 import React, {  useState } from 'react';
-import {  Stack, StackItem } from '@patternfly/react-core';
+import { Stack, StackItem, Text, TextContent, TextVariants } from '@patternfly/react-core';
 import { TopToolbar, TopToolbarTitle } from '../../presentational-components/shared/top-toolbar';
 import { Section } from '@redhat-cloud-services/frontend-components';
 import UsersList from '../group/add-group/users-list';
 
 const Users = () => {
   const [ selectedRows, setSelectedRows ] = useState([]);
+  const isProd = window.insights.chrome.isProd;
+  const description = (<TextContent>
+    <Text>
+    These are the users in your organization. To add new users to your organization go to{ ' ' }
+      <Text
+        component={ TextVariants.a }
+        href={ `https://www.${isProd ? '' : 'qa.'}redhat.com/wapps/ugc/protected/usermgt/userList.html` }>
+      user management list.
+      </Text>
+    </Text>
+  </TextContent>);
 
   const renderUsersList = () =>
     <Stack>
@@ -13,7 +24,7 @@ const Users = () => {
         <TopToolbar paddingBottm={ false }>
           <TopToolbarTitle
             title="Users"
-            description='These are the users in your organization. To add new users to your organization go to user management list.'
+            description={ description }
           />
         </TopToolbar>
       </StackItem>
