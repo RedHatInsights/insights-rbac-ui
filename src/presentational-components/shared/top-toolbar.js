85e9014ad9f73cf316d0468892f0dbbfd748f345
Ryan Long <rlong@redhat.com>
2020-02-17 15:54:38 -0500

    lint

--- src/presentational-components/shared/top-toolbar.js
+++ src/presentational-components/shared/top-toolbar.js
@@ -1,16 +1,16 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
-import { Level, Text, TextContent, TextVariants  } from '@patternfly/react-core';
+import { Text, TextContent, TextVariants  } from '@patternfly/react-core';
 import { ToolbarTitlePlaceholder } from './loader-placeholders';
 import RbacBreadcrumbs from './breadcrubms';
 import { PageHeader, PageHeaderTitle } from '@redhat-cloud-services/frontend-components';
 
 import './top-toolbar.scss';
 
-export const TopToolbar = ({ children,  breadcrumbs, hasTabs }) => (
+export const TopToolbar = ({ children,  breadcrumbs }) => (
   <Fragment>
     { breadcrumbs &&
-      <section class="pf-c-page__main-breadcrumb">
+      <section className="pf-c-page__main-breadcrumb">
         <RbacBreadcrumbs { ...breadcrumbs } />
       </section>
     }
@@ -35,7 +35,7 @@ TopToolbar.defaultProps = {
 
 export const TopToolbarTitle = ({ title, description, children }) => (
   <Fragment>
-    <PageHeaderTitle title={title} className='ins-rbac-page-header__title'/>
+    <PageHeaderTitle title={ title || <ToolbarTitlePlaceholder /> } className='ins-rbac-page-header__title'/>
     { description &&
       <TextContent className="ins-rbac-page-header__description">
         <Text component={ TextVariants.p }>{ description }</Text>
