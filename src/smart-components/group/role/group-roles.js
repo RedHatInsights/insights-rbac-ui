8240c34f23c9ab19a676131c75edbc3b20855e49
PanSpagetka <rknaur@redhat.com>
2020-02-19 14:22:46 +0100

    Don't show warning when default group was already edited

--- src/smart-components/group/role/group-roles.js
+++ src/smart-components/group/role/group-roles.js
@@ -214,7 +214,7 @@ const mapStateToProps = ({ groupReducer: { selectedGroup, groups }}) => {
     userIdentity: groups.identity,
     name: selectedGroup.name,
     isDefault: selectedGroup.platform_default,
-    isChanged: selectedGroup.system
+    isChanged: !selectedGroup.system
   };};
 
 const mapDispatchToProps = dispatch => {
