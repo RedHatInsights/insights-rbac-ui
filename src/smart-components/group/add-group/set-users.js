d9141023f9448698916fd0014ea5b26d0e861a4f
Karel Hala <khala@redhat.com>
2020-04-09 10:03:46 +0200

    Add shared component

--- src/smart-components/group/add-group/set-users.js
+++ src/smart-components/group/add-group/set-users.js
@@ -6,13 +6,12 @@ import {
   FormGroup,
   Stack,
   StackItem,
-  Text,
   TextContent,
-  TextVariants,
   Title
 } from '@patternfly/react-core';
 import UsersList from './users-list';
 import '../../../App.scss';
+import ActiveUser from '../../../presentational-components/shared/ActiveUsers';
 
 const SetUsers = ({ selectedUsers, setSelectedUsers, title, description }) => {
   return (
@@ -25,19 +24,13 @@ const SetUsers = ({ selectedUsers, setSelectedUsers, title, description }) => {
           <StackItem>
             <TextContent>
               <Title headingLevel="h4" size="xl"> Add members to the group </Title>
-              <Text
-                className="pf-u-mt-0"
-                component={ TextVariants.h6 }>
-                { description || <Fragment>
-                    Select users from your organization to add to this group. This list only shows active users; to see all users in your organization go to your{ ' ' }
-                    <Text
-                        component={ TextVariants.a }
-                        href={ `https://www.${window.insights.chrome.isProd ? '' : 'qa.'}redhat.com/wapps/ugc/protected/usermgt/userList.html` }>
-                    user management list.
-                    </Text>
-                </Fragment> }
-              </Text>
             </TextContent>
+            <ActiveUser
+              description={ (
+                description ||
+                'Select users from your organization to add to this group. This list only shows active users; to see all users in your organization go to your'
+              ) }
+            />
           </StackItem>
           <StackItem>
             <FormGroup
