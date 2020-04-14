975ea590c8e7f2f6e27dfa2a1f953409fdbdea66
Filip Hlavac <50696716+fhlavac@users.noreply.github.com>
2020-04-14 16:13:17 +0200

    Add item modal cancellation redesign (#262)
    * Restyle add ITEM cancellation modal
    
    * Update tests after add ITEM cancel modal restyling

--- src/smart-components/common/warningModal.js
+++ src/smart-components/common/warningModal.js
@@ -8,22 +8,21 @@ export const WarningModal = ({ type, isOpen, onModalCancel, onConfirmCancel }) =
   <Modal
     title={ <span className='ins-c-wizard__cancel-warning-header'>
         <ExclamationTriangleIcon size='md' className='ins-c-wizard__cancel-warning-header--icon'/>
-        Exit { type } creation
+        Exit { type } creation?
       </span> }
     isSmall
     className='ins-c-wizard__cancel-warning'
     isOpen={ isOpen }
     onClose={ onModalCancel }
     actions={ [
-      <Button key="confirm" variant="danger" onClick={ onConfirmCancel }>
-        Yes, I want to exit
+      <Button key="confirm" variant="primary" onClick={ onConfirmCancel }>
+        Exit
       </Button>,
       <Button key="cancel" variant="link" onClick={ onModalCancel }>
-        No, I want to continue
+        Stay
       </Button>
     ] }
     isFooterLeftAligned>
-    <span> Are you sure you want to stop creating a { type } in user access? </span>
     <span> All inputs will be discarded.</span>
   </Modal>
 );
