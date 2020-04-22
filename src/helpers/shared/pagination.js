52aaac4776a794e032d33d02448eb87709f99517
Filip Hlavac <filip.hlavac01@gmail.com>
2020-04-22 12:30:28 +0200

    Add default itemCount for table pagination

--- src/helpers/shared/pagination.js
+++ src/helpers/shared/pagination.js
@@ -1,11 +1,13 @@
 export const defaultSettings = {
   limit: 20,
-  offset: 0
+  offset: 0,
+  itemCount: 0
 };
 
 export const defaultCompactSettings = {
   limit: 10,
-  offset: 0
+  offset: 0,
+  itemCount: 0
 };
 
 export const getCurrentPage = (limit = 1, offset = 0) => Math.floor(offset / limit) + 1;
