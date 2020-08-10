fbe0127bdef5dba07f3ce74a5b6f63472f24d1fb
PanSpagetka <rknaur@redhat.com>
2020-08-10 12:07:44 +0200

    Cleanup

--- src/helpers/permission/permission-helper.js
+++ src/helpers/permission/permission-helper.js
@@ -2,7 +2,6 @@ import { getPermissionApi } from '../shared/user-login';
 
 const accessApi = getPermissionApi();
 
-// listPermissions: function (limit, offset, orderBy, application, resourceType, verb, permission, options) {
 export async function listPermissions(limit, offset, orderBy, application, resourceType, verb, permission, options) {
     return await accessApi.listPermissions(limit, offset, orderBy, application, resourceType, verb, permission, options);
 }
