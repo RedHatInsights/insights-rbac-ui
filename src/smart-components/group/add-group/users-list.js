1de45cbdeffce66e03ee54613db419448af74460
Martin Maroši <marvusm.mmi@gmail.com>
2020-09-02 10:51:39 +0200

    Allow filtering users by status.

--- src/smart-components/group/add-group/users-list.js
+++ src/smart-components/group/add-group/users-list.js
@@ -66,9 +66,10 @@ const createRows = (userLinks) => (data, _expanded, checkedRows = []) => {
 const UsersList = ({ users, fetchUsers, isLoading, pagination, selectedUsers, setSelectedUsers, userLinks, props }) => {
   const [filterValue, setFilterValue] = useState('');
   const [emailValue, setEmailValue] = useState('');
+  const [statusValue, setStatusValue] = useState(['active']);
 
   useEffect(() => {
-    fetchUsers();
+    fetchUsers(mappedProps({ ...defaultSettings, status: ['active'] }));
   }, []);
 
   const setCheckedItems = (newSelection) => {
@@ -85,13 +86,14 @@ const UsersList = ({ users, fetchUsers, isLoading, pagination, selectedUsers, se
       borders={false}
       createRows={createRows(userLinks)}
       data={users}
-      filterValue={filterValue}
       fetchData={(config) => {
-        fetchUsers(mappedProps(config));
+        const status = Object.prototype.hasOwnProperty.call(config, 'status') ? config.status : statusValue;
+        fetchUsers(mappedProps({ ...config, status }));
       }}
-      setFilterValue={({ username, email }) => {
+      setFilterValue={({ username, email, status }) => {
         typeof username !== 'undefined' && setFilterValue(username);
         typeof email !== 'undefined' && setEmailValue(email);
+        typeof statusValue !== undefined && setStatusValue(status);
       }}
       isLoading={isLoading}
       pagination={pagination}
@@ -107,6 +109,16 @@ const UsersList = ({ users, fetchUsers, isLoading, pagination, selectedUsers, se
       filters={[
         { key: 'username', value: filterValue, placeholder: 'Filter by exact username' },
         { key: 'email', value: emailValue, placeholder: 'Filter by exact email' },
+        {
+          key: 'status',
+          value: statusValue,
+          label: 'Status',
+          type: 'checkbox',
+          items: [
+            { label: 'Active', value: 'active' },
+            { label: 'Inactive', value: 'inactive' },
+          ],
+        },
       ]}
       {...props}
     />
