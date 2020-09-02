b6382443bf39fc5fd23cb25c8ff50ca413aeba41
Martin Maroši <marvusm.mmi@gmail.com>
2020-09-02 10:51:39 +0200

    Allow filtering users by status.

--- src/helpers/user/user-helper.js
+++ src/helpers/user/user-helper.js
@@ -2,9 +2,14 @@ import { getPrincipalApi } from '../shared/user-login';
 
 const principalApi = getPrincipalApi();
 
-export function fetchUsers({ limit, offset, username, orderBy, email }) {
+const principalStatusApiMap = {
+  active: 'enabled',
+  inactive: 'disabled',
+};
+export function fetchUsers({ limit, offset, username, orderBy, email, status = [] }) {
   const sortOrder = orderBy === '-username' ? 'desc' : 'asc';
-  return principalApi.listPrincipals(limit, offset, username, sortOrder, email).then(({ data, meta }) => {
+  const mappedStatus = status.length === 2 ? 'all' : principalStatusApiMap[status[0]];
+  return principalApi.listPrincipals(limit, offset, username, sortOrder, email, mappedStatus).then(({ data, meta }) => {
     return {
       data,
       meta: {
