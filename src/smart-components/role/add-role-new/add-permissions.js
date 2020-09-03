e561ccd80d22bdd4f9099af2610a4c5915d94e46
PanSpagetka <rknaur@redhat.com>
2020-09-03 09:30:48 +0200

    Fix deselecting permissions on filter

--- src/smart-components/role/add-role-new/add-permissions.js
+++ src/smart-components/role/add-role-new/add-permissions.js
@@ -109,7 +109,8 @@ const AddPermissionsTable = ({ selectedPermissions, setSelectedPermissions, ...p
     }, [ selectedPermissions ]);
 
     useEffect(() => {
-        if (baseRole) {
+        if (baseRole && roleType === 'copy') {
+
             setSelectedPermissions(() => resolveSplats(
                 (baseRole?.access || []).map(permission => ({ uuid: permission.permission })),
                 permissions.data
