1df292144ebd40f68d837e9d2a508956278ce71e
Martin Maroši <marvusm.mmi@gmail.com>
2020-09-16 10:14:08 +0200

    Add user cost management MUAAcessTable

--- src/smart-components/myUserAccess/mua-table-helpers.js
+++ src/smart-components/myUserAccess/mua-table-helpers.js
@@ -1,10 +1,24 @@
-export const createRows = (data) =>
-  data.reduce((acc, { permission }) => {
+import React, { Fragment } from 'react';
+import ResourceDefinitionsButton from '../../presentational-components/myUserAccess/ResourceDefinitionsButton';
+
+export const createRows = (data, showResourceDefinitions, onRdClick) =>
+  data.reduce((acc, { permission, ...access }, index) => {
     const [appName, type, operation] = permission.split(':');
     return [
       ...acc,
       {
-        cells: [appName, type, operation],
+        cells: [
+          appName,
+          type,
+          operation,
+          ...(showResourceDefinitions
+            ? [
+                <Fragment key="rd">
+                  <ResourceDefinitionsButton onClick={() => onRdClick(index)} access={access} />
+                </Fragment>,
+              ]
+            : []),
+        ],
       },
     ];
   }, []);
