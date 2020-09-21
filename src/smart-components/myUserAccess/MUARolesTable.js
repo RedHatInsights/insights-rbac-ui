a19a5547cb8db8df47670598fd5febaf39b907a1
Martin Maroši <marvusm.mmi@gmail.com>
2020-09-21 08:56:32 +0200

    Fix roles table expansion after linter updates.

--- src/smart-components/myUserAccess/MUARolesTable.js
+++ src/smart-components/myUserAccess/MUARolesTable.js
@@ -39,10 +39,6 @@ const MUARolesTable = ({
     fetchRoles({ limit: 20, offset: 0, scope: 'principal', application: apps.join(',') });
   }, []);
 
-  useEffect(() => {
-    fetchRoles({ limit: 20, offset: 0, scope: 'principal' });
-  }, []);
-
   const createRows = (data) => {
     return data?.reduce(
       (acc, { uuid, name, description, accessCount }, i) => [
@@ -61,44 +57,41 @@ const MUARolesTable = ({
           compoundParent: 2,
           cells: [
             {
-              uuid: `${uuid}-groups`,
-              parent: 2 * i,
-              compoundParent: 2,
-              cells: [
-                {
-                  props: { colSpan: 4, className: 'pf-m-no-padding' },
-                  title: rolesWithAccess?.[uuid] ? (
-                    <Table
-                      aria-label="Simple Table"
-                      borders={false}
-                      variant={TableVariant.compact}
-                      cells={['Application', 'Resource type', 'Operation', ...(showResourceDefinitions ? ['Resource definitions'] : [])]}
-                      rows={rolesWithAccess[uuid].access.map((access) => ({
-                        cells: [
-                          ...access.permission.split(':'),
-                          ...(showResourceDefinitions
-                            ? [
-                                <Fragment key="rd">
-                                  <ResourceDefinitionsButton
-                                    onClick={() =>
-                                      setRdConfig({ rdOpen: true, rdPermission: access.permission, resourceDefinitions: access.resourceDefinitions })
-                                    }
-                                    access={access}
-                                  />
-                                </Fragment>,
-                              ]
-                            : []),
-                        ],
-                      }))}
-                    >
-                      <TableHeader />
-                      <TableBody />
-                    </Table>
-                  ) : (
-                    <ListLoader />
-                  ),
-                },
-              ],
+              props: { colSpan: 4, className: 'pf-m-no-padding' },
+              title: rolesWithAccess?.[uuid] ? (
+                <Table
+                  aria-label="Simple Table"
+                  borders={false}
+                  variant={TableVariant.compact}
+                  cells={['Application', 'Resource type', 'Operation', ...(showResourceDefinitions ? ['Resource definitions'] : [])]}
+                  rows={rolesWithAccess[uuid].access.map((access) => ({
+                    cells: [
+                      ...access.permission.split(':'),
+                      ...(showResourceDefinitions
+                        ? [
+                            <Fragment key="rd">
+                              <ResourceDefinitionsButton
+                                onClick={() =>
+                                  setRdConfig({
+                                    rdOpen: true,
+                                    rdPermission: access.permission,
+                                    resourceDefinitions: access.resourceDefinitions,
+                                  })
+                                }
+                                access={access}
+                              />
+                            </Fragment>,
+                          ]
+                        : []),
+                    ],
+                  }))}
+                >
+                  <TableHeader />
+                  <TableBody />
+                </Table>
+              ) : (
+                <ListLoader />
+              ),
             },
           ],
         },
@@ -117,13 +110,13 @@ const MUARolesTable = ({
 
   const onExpand = (_event, _rowIndex, colIndex, isOpen, rowData) => {
     if (!isOpen) {
-      setExpanded({ ...expanded, [rowData.uuid]: colIndex });
+      setExpanded((expanded) => ({ ...expanded, [rowData.uuid]: colIndex }));
       // Permissions
       if (colIndex === 2) {
         fetchRoleForPrincipal(rowData.uuid);
       }
     } else {
-      setExpanded({ ...expanded, [rowData.uuid]: -1 });
+      setExpanded((expanded) => ({ ...expanded, [rowData.uuid]: -1 }));
     }
   };
 
