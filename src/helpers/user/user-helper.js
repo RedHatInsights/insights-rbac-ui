2e05605b96bbe55d0bdaa1ba527139be1967ab87
Martin Maroši <marvusm.mmi@gmail.com>
2020-10-02 08:35:26 +0200

    Capitalize users list status chips.

--- src/helpers/user/user-helper.js
+++ src/helpers/user/user-helper.js
@@ -3,12 +3,12 @@ import { getPrincipalApi } from '../shared/user-login';
 const principalApi = getPrincipalApi();
 
 const principalStatusApiMap = {
-  active: 'enabled',
-  inactive: 'disabled',
+  Active: 'enabled',
+  Inactive: 'disabled',
 };
 export function fetchUsers({ limit, offset, username, orderBy, email, status = [] }) {
   const sortOrder = orderBy === '-username' ? 'desc' : 'asc';
-  const mappedStatus = status.length === 2 ? 'all' : principalStatusApiMap[status[0]];
+  const mappedStatus = status.length === 2 ? 'all' : principalStatusApiMap[status[0]] || 'all';
   return principalApi.listPrincipals(limit, offset, username, sortOrder, email, mappedStatus).then(({ data, meta }) => {
     return {
       data,
