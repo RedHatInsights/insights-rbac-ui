e1bf2290c87da3b352cc6c36b2bc006f5d567e30
PanSpagetka <rknaur@redhat.com>
2020-10-09 10:06:57 +0200

    Add cost API

--- src/helpers/shared/user-login.js
+++ src/helpers/shared/user-login.js
@@ -1,19 +1,20 @@
 /* eslint-disable no-unused-vars */
 import axiosInstance from '@redhat-cloud-services/frontend-components-utilities/files/interceptors';
 import { GroupApi, PrincipalApi, RoleApi, PolicyApi, AccessApi, PermissionApi } from '@redhat-cloud-services/rbac-client';
-// import { CostModelApi } from '@redhat-cloud-services/cost-management-client';
+import { BaseAPI } from '@redhat-cloud-services/rbac-client/dist/base';
 
 import { RBAC_API_BASE, COST_API_BASE } from '../../utilities/constants';
 
+console.log('BASE API', BaseAPI);
 const principalApi = new PrincipalApi(undefined, RBAC_API_BASE, axiosInstance);
 const groupApi = new GroupApi(undefined, RBAC_API_BASE, axiosInstance);
 const roleApi = new RoleApi(undefined, RBAC_API_BASE, axiosInstance);
 const policyApi = new PolicyApi(undefined, RBAC_API_BASE, axiosInstance);
 const accessApi = new AccessApi(undefined, RBAC_API_BASE, axiosInstance);
 const permissionApi = new PermissionApi(undefined, RBAC_API_BASE, axiosInstance);
-// const api = new CostModelApi(undefined, undefined, axiosInstance);
-
-// const costApi = new CostModelApi();
+const costApi = new BaseAPI(undefined, COST_API_BASE, axiosInstance);
+console.log('COST API', costApi);
+console.log('ROLE API', roleApi);
 
 export function getPrincipalApi() {
   return principalApi;
@@ -43,6 +44,9 @@ export function getAxiosInstance() {
   return axiosInstance;
 }
 
-// export async function getCostApi() {
-//   return await costApi.getCostModel();
-// }
+export function getCostApi() {
+  return {
+    getResourceTypes: () => costApi.axios.get(`${COST_API_BASE}/resource-types/`),
+    getResource: (path) => costApi.axios.get(`${path}`),
+  };
+}
