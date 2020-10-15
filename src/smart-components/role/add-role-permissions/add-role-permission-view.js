f98aa93e1baa2ad7725bde48c262571a353d44b0
Alexander Rivera <hq@aleriver@redhat.com-mac>
2020-10-15 10:09:19 -0400

    add

--- src/smart-components/role/add-role-permissions/add-role-permission-view.js
+++ src/smart-components/role/add-role-permissions/add-role-permission-view.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 // import { usePermissions } from '@redhat-cloud-services/frontend-components-utilities/files/RBACHook';
 
-// This component takes care of pulling down active permissions available to be added to the current role in focus. 
+// This component takes care of pulling down active permissions available to be added to the current role in focus.
 const AddRolePermissionView = () => {
   const [flag, setFlag] = useState(true);
   console.log('Testing out my flag in add-role-permission-view!', flag);
