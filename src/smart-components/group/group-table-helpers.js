b66060c7161c04682217d94949cc423cf2dae7ea
Martin Maroši <marvusm.mmi@gmail.com>
2020-11-19 11:36:33 +0100

    Use ref for popover root instead of HTML element. (#588)
    Co-authored-by: Karel Hala <khala@redhat.com>

--- src/smart-components/group/group-table-helpers.js
+++ src/smart-components/group/group-table-helpers.js
@@ -1,12 +1,40 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { DateFormat } from '@redhat-cloud-services/frontend-components';
 import { OutlinedQuestionCircleIcon } from '@patternfly/react-icons';
 import { Popover } from '@patternfly/react-core';
 import classNames from 'classnames';
+import PropTypes from 'prop-types';
+
+const DefaultPlatformPopover = ({ uuid }) => {
+  const [isPopoverVisible, setPopoverVisible] = useState(false);
+  const popoverRootRef = useRef(null);
+
+  return (
+    <span ref={popoverRootRef} key={`${uuid}-popover`} id="default-group-popover">
+      <Popover
+        zIndex="110"
+        position="right"
+        isVisible={isPopoverVisible}
+        shouldClose={() => setPopoverVisible(false)}
+        hideOnOutsideClick
+        bodyContent="This group contains the roles that all users in your organization inherit by default."
+        appendTo={popoverRootRef.current}
+      >
+        <OutlinedQuestionCircleIcon
+          onClick={() => setPopoverVisible(!isPopoverVisible)}
+          className={classNames('pf-c-question-circle-icon', { 'icon-active': isPopoverVisible })}
+        />
+      </Popover>
+    </span>
+  );
+};
+
+DefaultPlatformPopover.propTypes = {
+  uuid: PropTypes.string.isRequired,
+};
 
 export const createRows = (data, opened, selectedRows = []) => {
-  const [isPopoverVisible, setPopoverVisible] = useState(false);
   return data.reduce(
     (acc, { uuid, name, roleCount, principalCount, modified, platform_default: isPlatformDefault }) => [
       ...acc,
@@ -19,24 +47,7 @@ export const createRows = (data, opened, selectedRows = []) => {
               <Link key={`${uuid}-link`} to={`/groups/detail/${uuid}`}>
                 {name}
               </Link>
-              {isPlatformDefault && (
-                <span key={`${uuid}-popover`} id="default-group-popover">
-                  <Popover
-                    zIndex="110"
-                    position="right"
-                    isVisible={isPopoverVisible}
-                    shouldClose={() => setPopoverVisible(false)}
-                    hideOnOutsideClick={true}
-                    bodyContent="This group contains the roles that all users in your organization inherit by default."
-                    appendTo={document.getElementById('default-group-popover')}
-                  >
-                    <OutlinedQuestionCircleIcon
-                      onClick={() => setPopoverVisible(!isPopoverVisible)}
-                      className={classNames('pf-c-question-circle-icon', { 'icon-active': isPopoverVisible })}
-                    />
-                  </Popover>
-                </span>
-              )}
+              {isPlatformDefault && <DefaultPlatformPopover uuid={uuid} />}
             </div>
           </Fragment>,
           roleCount,
