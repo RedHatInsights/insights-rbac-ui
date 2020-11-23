0929f6db121f27d4db070db2d669b698e28a6da5
PanSpagetka <rknaur@redhat.com>
2020-11-23 14:00:44 +0100

    Change wording and use relative link

--- src/smart-components/role/add-role-new/DisabledRowWrapper.js
+++ src/smart-components/role/add-role-new/DisabledRowWrapper.js
@@ -8,8 +8,8 @@ export const DisabledRowWrapper = ({ row, ...props }) =>
     <Tooltip
       content={
         <div>
-          To add this permission to your role and define specific resources for it, at least one data source must be connected. Go{' '}
-          <a href="https://cloud.redhat.com/settings/sources">here</a> to add a new Source for Cost Management.
+          To add this permission to your role and define specific resources for it, at least one data source must be connected.{' '}
+          <a href="./settings/sources">Configure sources for Cost Management</a>
         </div>
       }
       exitDelay={2000}
