f3add023b78aa7fd114ef7d730a8f5d361a976c9
Karel Hala <khala@redhat.com>
2021-02-17 16:32:16 +0100

    Update deps and config to use module in MUA

--- config/dev.webpack.config.js
+++ config/dev.webpack.config.js
@@ -5,9 +5,15 @@ const config = require('@redhat-cloud-services/frontend-components-config');
 const { config: webpackConfig, plugins } = config({
   rootFolder: resolve(__dirname, '../'),
   debug: true,
+  https: true,
+  sassPrefix: '.rbac, .my-user-access',
 });
 
-plugins.push(require('./federation-plugin'));
+plugins.push(
+  require('@redhat-cloud-services/frontend-components-config/federated-modules')({
+    root: resolve(__dirname, '../'),
+  })
+);
 
 module.exports = (env) => {
   env && env.analyze === 'true' && plugins.push(new BundleAnalyzerPlugin());
--- config/prod.webpack.config.js
+++ config/prod.webpack.config.js
@@ -3,10 +3,14 @@ const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPl
 const config = require('@redhat-cloud-services/frontend-components-config');
 const { config: webpackConfig, plugins } = config({
   rootFolder: resolve(__dirname, '../'),
-  mode: 'production',
+  sassPrefix: '.rbac, .my-user-access',
 });
 
-plugins.push(require('./federation-plugin'));
+plugins.push(
+  require('@redhat-cloud-services/frontend-components-config/federated-modules')({
+    root: resolve(__dirname, '../'),
+  })
+);
 
 module.exports = (env) => {
   env && env.analyze === 'true' && plugins.push(new BundleAnalyzerPlugin());
