f8d94455324bf0a164f4a10313e1be9ea63d0b90
Filip Hlavac <filip.hlavac01@gmail.com>
2021-04-26 15:35:51 +0200

    Update generic functions for pagination

--- src/helpers/shared/pagination.js
+++ src/helpers/shared/pagination.js
@@ -10,17 +10,17 @@ export const defaultCompactSettings = {
   itemCount: 0,
 };
 
-export const getCurrentPage = (limit = 1, offset = 0) => Math.floor(offset / limit) + 1;
+export const calculatePage = (limit = defaultSettings.limit, offset = 0) => Math.floor(offset / limit) + 1;
 
-export const getNewPage = (page = 1, offset) => (page - 1) * offset;
+export const calculateOffset = (page = 1, limit = defaultSettings.limit) => (page - 1) * limit;
 
-export const syncDefaultPaginationWithUrl = (history, defaultPagination, initialLoad) => {
+export const syncDefaultPaginationWithUrl = (history, defaultPagination = defaultSettings) => {
   const searchParams = new URLSearchParams(history.location.search);
 
   isNaN(parseInt(searchParams.get('per_page'))) && searchParams.set('per_page', defaultPagination.limit);
   const limit = parseInt(searchParams.get('per_page'));
-  isNaN(parseInt(searchParams.get('page'))) && searchParams.set('page', initialLoad ? 1 : defaultPagination.offset / limit + 1);
-  const offset = (parseInt(searchParams.get('page')) - 1) * limit;
+  isNaN(parseInt(searchParams.get('page'))) && searchParams.set('page', 1);
+  const offset = calculateOffset(parseInt(searchParams.get('page')), limit);
 
   history.replace({
     pathname: history.location.pathname,
@@ -32,7 +32,7 @@ export const syncDefaultPaginationWithUrl = (history, defaultPagination, initial
 export const applyPaginationToUrl = (history, limit, offset = 0) => {
   const searchParams = new URLSearchParams(history.location.search);
   searchParams.set('per_page', limit);
-  searchParams.set('page', offset / limit + 1);
+  searchParams.set('page', calculatePage(limit, offset));
 
   history.replace({
     pathname: history.location.pathname,
