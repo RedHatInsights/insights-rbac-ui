c3791ec135f122ce6057439ae8aa2ac756079173
Filip Hlavac <filip.hlavac01@gmail.com>
2021-07-27 10:54:28 +0200

    Fix the user-management link

--- src/presentational-components/shared/ActiveUsers.js
+++ src/presentational-components/shared/ActiveUsers.js
@@ -2,21 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Text, TextVariants } from '@patternfly/react-core';
 import { ExternalLinkAltIcon } from '@patternfly/react-icons';
-const ActiveUser = ({ description, linkTitle }) => (
-  <Text className="pf-u-mt-0" component={TextVariants.h7}>
-    {description}
-    <Text
-      component={TextVariants.a}
-      href={`https://www.${window.insights.chrome.isProd ? '' : 'qa.'}redhat.com/wapps/ugc/protected/usermgt/userList.html`}
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      {linkTitle}
-      <ExternalLinkAltIcon />
+const ActiveUser = ({ description, linkTitle }) => {
+  const env = insights.chrome.getEnvironment();
+  const prefix = insights.chrome.isProd ? '' : `${env === 'ci' ? 'qa' : env}.`;
+  return (
+    <Text className="pf-u-mt-0" component={TextVariants.h7}>
+      {description}
+      <Text
+        component={TextVariants.a}
+        href={`https://www.${prefix}redhat.com/wapps/ugc/protected/usermgt/userList.html`}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {linkTitle}
+        <ExternalLinkAltIcon />
+      </Text>
+      .
     </Text>
-    .
-  </Text>
-);
+  );
+};
 
 ActiveUser.propTypes = {
   description: PropTypes.node,
