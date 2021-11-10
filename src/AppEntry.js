b490cd568a8d06412774c9dfd60f33d7faa16f7a
Martin Maroši <marvusm.mmi@gmail.com>
2021-11-10 14:26:40 +0100

    Jenkins trigger

--- src/AppEntry.js
+++ src/AppEntry.js
@@ -5,20 +5,18 @@ import registry, { RegistryContext } from './utilities/store';
 import App from './App';
 import { getBaseName } from '@redhat-cloud-services/frontend-components-utilities/helpers';
 
-const InsightsRbac = () => {
-  return (
-    <RegistryContext.Provider
-      value={{
-        getRegistry: () => registry,
-      }}
-    >
-      <Provider store={registry.getStore()}>
-        <Router basename={getBaseName(location.pathname, 2).includes('rbac') ? getBaseName(location.pathname, 2) : getBaseName(location.pathname, 1)}>
-          <App />
-        </Router>
-      </Provider>
-    </RegistryContext.Provider>
-  );
-};
+const InsightsRbac = () => (
+  <RegistryContext.Provider
+    value={{
+      getRegistry: () => registry,
+    }}
+  >
+    <Provider store={registry.getStore()}>
+      <Router basename={getBaseName(location.pathname, 2).includes('rbac') ? getBaseName(location.pathname, 2) : getBaseName(location.pathname, 1)}>
+        <App />
+      </Router>
+    </Provider>
+  </RegistryContext.Provider>
+);
 
 export default InsightsRbac;
