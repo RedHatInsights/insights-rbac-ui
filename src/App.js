e8fbed7287e56a07b32c2bad6652d75420542401
Martin Maroši <marvusm.mmi@gmail.com>
2021-11-22 17:31:43 +0100

    Stop checking org admin (#982)
    * Remove org admin check.
    
    * Convert Rbac root to function component.

--- src/App.js
+++ src/App.js
@@ -1,70 +1,59 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import { withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import { Routes } from './routes';
 import Main from '@redhat-cloud-services/frontend-components/Main';
 import NotificationPortal from '@redhat-cloud-services/frontend-components-notifications/NotificationPortal/';
 import { AppPlaceholder } from './presentational-components/shared/loader-placeholders';
 import { IntlProvider } from 'react-intl';
-import DeniedState from './presentational-components/states/DeniedState';
 import ErroReducerCatcher from './presentational-components/shared/ErrorReducerCatcher';
 
 import './App.scss';
 
-class App extends Component {
-  state = {
-    chromeNavAvailable: true,
-    userReady: false,
-    isAdmin: undefined,
-  };
+const App = () => {
+  const [userReady, setUserReady] = useState(false);
+  const history = useHistory();
 
-  componentDidMount() {
-    const { history } = this.props;
+  useEffect(() => {
     insights.chrome.init();
     insights.chrome.registerModule('access-requests');
     !insights.chrome.getApp() && history.push('/my-user-access'); // redirect to MUA if url is "/settings"
-    insights.chrome.auth.getUser().then((user) => this.setState({ userReady: true, isAdmin: user.identity.user.is_org_admin }));
+    insights.chrome.auth.getUser().then(() => setUserReady(true));
     insights.chrome.identifyApp(insights.chrome.getApp());
-    this.unregister = insights.chrome.on('APP_NAVIGATION', (event) => {
+
+    const unregister = insights.chrome.on('APP_NAVIGATION', (event) => {
       if (event.domEvent) {
         history.push(`/${event.navId}`);
       }
     });
+
+    return () => {
+      if (typeof unregister === 'function') {
+        unregister();
+      }
+    };
+  });
+
+  if (!userReady) {
+    return <AppPlaceholder />;
   }
 
-  componentWillUnmount() {
-    this.unregister && this.unregister();
-  }
-
-  render() {
-    const { userReady, isAdmin } = this.state;
-
-    if (!userReady) {
-      return <AppPlaceholder />;
-    }
-
-    if (!isAdmin && insights.chrome.getApp() === 'rbac') {
-      return <DeniedState />;
-    }
-
-    return (
-      <IntlProvider locale="en">
-        <React.Fragment>
-          <NotificationPortal />
-          <ErroReducerCatcher>
-            <Main style={{ marginLeft: 0, padding: 0 }}>
-              <Routes />
-            </Main>
-          </ErroReducerCatcher>
-        </React.Fragment>
-      </IntlProvider>
-    );
-  }
-}
+  return (
+    <IntlProvider locale="en">
+      <React.Fragment>
+        <NotificationPortal />
+        <ErroReducerCatcher>
+          <Main style={{ marginLeft: 0, padding: 0 }}>
+            <Routes />
+          </Main>
+        </ErroReducerCatcher>
+      </React.Fragment>
+    </IntlProvider>
+  );
+};
 
 App.propTypes = {
   history: PropTypes.object,
 };
 
-export default withRouter(connect()(App));
+export default App;
