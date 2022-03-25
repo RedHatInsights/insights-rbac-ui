f5d84c1b8a18e3d967e7b6c169c1505a041d36df
Martin Maroši <marvusm.mmi@gmail.com>
2022-03-25 12:01:13 +0100

    Help topics usage showcase

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import { Route, Switch, Redirect } from 'react-router-dom';
-import React, { lazy, Suspense } from 'react';
+import React, { lazy, Suspense, useContext, useEffect, useState } from 'react';
 import { AppPlaceholder } from './presentational-components/shared/loader-placeholders';
 import pathnames from './utilities/pathnames';
 import QuickstartsTestButtons from './utilities/quickstarts-test-buttons';
@@ -11,9 +11,20 @@ const MyUserAccess = lazy(() => import('./smart-components/myUserAccess/MUAHome'
 const AccessRequests = lazy(() => import('./smart-components/accessRequests/accessRequests'));
 const QuickstartsTest = lazy(() => import('./smart-components/quickstarts/quickstarts-test'));
 
+import { HelpTopicContext } from '@patternfly/quickstarts';
+import useChrome from '@redhat-cloud-services/frontend-components/useChrome';
+
 export const Routes = () => {
+  const {
+    helpTopics: { setActiveTopic, closeHelpTopic },
+  } = useChrome();
+  const { activeHelpTopic } = useContext(HelpTopicContext);
+
   return (
     <Suspense fallback={<AppPlaceholder />}>
+      <button onClick={() => (activeHelpTopic ? closeHelpTopic() : setActiveTopic('workspace'))}>
+        {activeHelpTopic ? 'Close help topic drawer' : 'Open workspace help topic'}
+      </button>
       <QuickstartsTestButtons />
       <Switch>
         <Route path={pathnames.groups} component={Groups} />
