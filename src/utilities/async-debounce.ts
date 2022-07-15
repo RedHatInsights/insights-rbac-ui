927c0690c046efe53dde2cff7b82d6db393b3cb7
Martin Maroši <marvusm.mmi@gmail.com>
2022-07-15 14:17:23 +0200

    Migrate async-debounce module to ts

--- src/utilities/async-debounce.ts
+++ src/utilities/async-debounce.ts
@@ -1,4 +1,4 @@
 import awesomeDebouncePromise from 'awesome-debounce-promise';
 
-export default (asyncFunction, debounceTime = 250, options = { onlyResolvesLast: false }) =>
+export default (asyncFunction: (...args: any[]) => Promise<any> , debounceTime = 250, options = { onlyResolvesLast: false }) =>
   awesomeDebouncePromise(asyncFunction, debounceTime, options);
