c1c202128a389a4bf62befce11cff5a8168e6e2e
Filip Hlavac <filip.hlavac01@gmail.com>
2022-10-19 16:06:40 +0200

    Allow 100 rows per page

--- src/presentational-components/shared/toolbar.js
+++ src/presentational-components/shared/toolbar.js
@@ -25,6 +25,7 @@ export const paginationBuilder = (pagination = {}, fetchData = () => undefined, 
     { title: '10', value: 10 },
     { title: '20', value: 20 },
     { title: '50', value: 50 },
+    { title: '100', value: 100 },
   ],
   onPerPageSelect: (_event, perPage) => {
     fetchData({
