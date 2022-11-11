2c94643115f1ee7d1d22a935a9ed8703cc16e97e
Filip Hlavac <50696716+fhlavac@users.noreply.github.com>
2022-11-11 21:13:58 +0100

    Revert "Update master stable"

---
+++ config/dev.webpack.config.js
@@ -0,0 +1,47 @@
+/* global */
+const webpack = require('webpack');
+const { resolve } = require('path');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const config = require('@redhat-cloud-services/frontend-components-config');
+
+const insightsProxy = {
+  https: false,
+  ...(process.env.BETA && { deployment: 'beta/apps' }),
+};
+
+const webpackProxy = {
+  deployment: process.env.BETA ? 'beta/apps' : 'apps',
+  useProxy: true,
+  proxyVerbose: true,
+  env: `${process.env.ENVIRONMENT || 'stage'}-${process.env.BETA ? 'beta' : 'stable'}`,
+  appUrl: process.env.BETA ? ['/beta/settings/my-user-access', '/beta/settings/rbac'] : ['/settings/my-user-access', '/settings/rbac'],
+};
+
+const { config: webpackConfig, plugins } = config({
+  rootFolder: resolve(__dirname, '../'),
+  debug: true,
+  sassPrefix: '.rbac, .my-user-access',
+  client: { overlay: false },
+  useCache: true,
+  useFileHash: false,
+  ...(process.env.PROXY ? webpackProxy : insightsProxy),
+});
+
+plugins.push(
+  require('@redhat-cloud-services/frontend-components-config/federated-modules')({
+    root: resolve(__dirname, '../'),
+    useFileHash: false,
+  })
+);
+
+plugins.push(
+  new webpack.DefinePlugin({
+    API_BASE: JSON.stringify('/api/rbac/v1'),
+  })
+);
+
+module.exports = (env) => {
+  env && env.analyze === 'true' && plugins.push(new BundleAnalyzerPlugin());
+
+  return { ...webpackConfig, plugins };
+};
