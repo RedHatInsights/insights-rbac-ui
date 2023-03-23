ebbb269bcd82e11bf8ad380621d7b5590fa3bc6e
Alexander Rivera <xanverapr@gmail.com>
2023-03-23 10:24:18 -0400

    Readded and updated snapshot testing to new test suite.

--- src/test/presentional-components/myUserAccess/MUACard.test.js
+++ src/test/presentional-components/myUserAccess/MUACard.test.js
@@ -16,32 +16,37 @@ const mockHeader = 'TestHeader';
 
 describe('<MUACard />', () => {
   test('Should render correctly if entitled', () => {
-    render(
+    const { container } = render(
       <RouterWrapper>
         <MUACard header={mockHeader} entitlements={entitlementsMock} />
       </RouterWrapper>
     );
-
+    
+    expect(container.firstChild).toMatchSnapshot();
     expect(screen.getByText(mockHeader)).toBeInTheDocument();
   });
 
   test('Should render, but being disabled', () => {
-    render(
+    const { container } = render(
       <RouterWrapper>
         <MUACard header={mockHeader} entitlements={entitlementsMock} isDisabled />
       </RouterWrapper>
     );
+
+    expect(container.firstChild).toMatchSnapshot();
 
     const cardLinks = screen.getAllByLabelText('card-link');
     expect(cardLinks[0]).toHaveClass('rbac-c-mua-bundles__cardlink rbac-c-mua-bundles__cardlink--disabled');
   });
 
   test('Should not render when unentitled', () => {
-    render(
+    const { container } = render(
       <RouterWrapper>
         <MUACard header={mockHeader} entitlements={[]} />
       </RouterWrapper>
     );
+
+    expect(container.firstChild).toMatchSnapshot();
 
     const cardLinks = screen.queryAllByRole('link', { name: /cardlink/i });
     expect(cardLinks).toHaveLength(0);
