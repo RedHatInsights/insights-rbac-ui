498dbe6f48e124f79658029e4d6bf7c051ad6e61
Alexander Rivera <xanverapr@gmail.com>
2023-03-29 11:46:06 -0400

    Fixed user interaction test, added new test case.

--- src/test/smart-components/user/addUserToGroup.test.js
+++ src/test/smart-components/user/addUserToGroup.test.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Provider, useSelector } from 'react-redux';
-import { render, screen, waitFor } from '@testing-library/react'; 
-import { MemoryRouter, useHistory} from 'react-router-dom';
-import { addNotification } from '@redhat-cloud-services/frontend-components-notifications/';
+import { Provider } from 'react-redux';
+import { render, screen, waitFor } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import AddUserToGroup from '../../../smart-components/user/add-user-to-group/add-user-to-group';
 import * as groupActions from '../../../redux/actions/group-actions';
 import userEvent from '@testing-library/user-event';
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import { FETCH_GROUPS, ADD_MEMBERS_TO_GROUP } from '../../../redux/action-types.js';
+import PermissionsContext from '../../../utilities/permissions-context';
+import messages from '../../../Messages';
 
 const mockStore = configureMockStore([thunk]);
 const initialState = {
@@ -20,11 +21,18 @@ const initialState = {
   },
 };
 
-const renderComponent = (userName = 'testuser', store) => {
+const renderComponent = (userName = 'testuser', store, isAdmin = true) => {
   return render(
     <Provider store={store}>
       <MemoryRouter>
-        <AddUserToGroup username={userName} />
+        <PermissionsContext.Provider
+          value={{
+            userAccessAdministrator: isAdmin,
+            orgAdmin: isAdmin,
+          }}
+        >
+          <AddUserToGroup username={userName} />
+        </PermissionsContext.Provider>
       </MemoryRouter>
     </Provider>
   );
@@ -32,23 +40,23 @@ const renderComponent = (userName = 'testuser', store) => {
 
 const testGroups = [
   {
-     uuid: '1',
-     name: 'Group 1',
-     description: "The best group",
+    uuid: '1',
+    name: 'Group 1',
+    description: 'The best group',
   },
   {
     uuid: '2',
     name: 'Group 2',
     description: 'The second best',
-  }
+  },
 ];
 
 describe('Add User to Group Wizard', () => {
   let fetchGroups, addMembersToGroup, store;
-  
+
   beforeEach(() => {
     jest.clearAllMocks();
-  
+
     fetchGroups = jest.spyOn(groupActions, 'fetchGroups');
     addMembersToGroup = jest.spyOn(groupActions, 'addMembersToGroup');
 
@@ -63,7 +71,7 @@ describe('Add User to Group Wizard', () => {
     });
 
     fetchGroups.mockImplementationOnce(() => ({
-     type: FETCH_GROUPS,
+      type: FETCH_GROUPS,
       payload: Promise.resolve({
         value: {
           data: testGroups,
@@ -75,7 +83,6 @@ describe('Add User to Group Wizard', () => {
         },
       }),
     }));
-   
   });
 
   test('Add to group wizard renders correctly', () => {
@@ -90,26 +97,40 @@ describe('Add User to Group Wizard', () => {
     renderComponent('testuser', store);
     expect(fetchGroups).toHaveBeenCalled();
   });
-  
+
   test('User groups table is rendered, with fetch group action dispatched', async () => {
     renderComponent('testUser', store);
 
-
     expect(fetchGroups).toHaveBeenCalled();
     await waitFor(() => expect(screen.getByLabelText('groups table')));
+    await waitFor(() => expect(screen.getByText(`${messages.onlyNonUserGroupsVisible.defaultMessage}`)));
     screen.debug();
+  });
+
+  test('Cannot add to group if the user is non-admin', () => {
+    renderComponent('testUser', store, false);
+
+    expect(screen.queryByLabelText('Select row 0')).toBeNull();
+    expect(screen.queryByLabelText('Save')).toBeDisabled();
   });
 
   test('Can select group and add user to it', async () => {
+    addMembersToGroup.mockImplementationOnce(() => ({
+      type: ADD_MEMBERS_TO_GROUP,
+      payload: Promise.resolve({
+        value: {
+          groupUuid: '1',
+          userUuid: 'testUser',
+        },
+      }),
+    }));
     renderComponent('testUser', store);
 
     await waitFor(() => expect(screen.getByLabelText(`group-name-${testGroups[0].uuid}`)));
-    // userEvent.click(screen.getByLabelText(`Select row 0`));
-    userEvent.click(screen.getByRole('checkbox', { name: `checkrow${0}` }));
+    userEvent.click(screen.getByLabelText(`Select row 0`));
     userEvent.click(screen.getByLabelText(`Save`));
 
-    expect(addMembersToGroup).toHaveBeenCalled();
+    await waitFor(() => expect(addMembersToGroup).toHaveBeenCalled());
     screen.debug();
   });
 });
-
