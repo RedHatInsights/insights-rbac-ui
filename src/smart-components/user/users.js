165718c1aac1fe705f6d33ee8dbef20f117e48c8
Martin Maroši <marvusm.mmi@gmail.com>
2023-06-06 14:12:14 +0200

    Enable eslint in users component.

--- src/smart-components/user/users.js
+++ src/smart-components/user/users.js
@@ -1,9 +1,8 @@
-/* eslint-disable */
 import React, { useEffect, useContext } from 'react';
 import { Switch } from 'react-router-dom';
 import { Stack, StackItem } from '@patternfly/react-core';
 import { TopToolbar, TopToolbarTitle } from '../../presentational-components/shared/top-toolbar';
-import { Section } from '@redhat-cloud-services/frontend-components';
+import Section from '@redhat-cloud-services/frontend-components/Section';
 import UsersList from '../group/add-group/users-list';
 import ActiveUser from '../../presentational-components/shared/ActiveUsers';
 import User from './user';
@@ -17,53 +16,39 @@ const Users = () => {
   const intl = useIntl();
   const activeUserPermissions = useContext(PermissionsContext);
 
-  const description = (
-    <ActiveUser
-      linkDescription={intl.formatMessage(messages.addNewUsersText)}
-    />
-  );
+  const description = <ActiveUser linkDescription={intl.formatMessage(messages.addNewUsersText)} />;
 
   useEffect(() => {
     insights.chrome.appNavClick({ id: 'users', secondaryNav: true });
   }, []);
 
-  const renderUsers = () => <Stack >
-    <StackItem>
-      <TopToolbar>
-        <TopToolbarTitle
-          title={intl.formatMessage(messages.users)}
-          description={ description }
-        />
-      </TopToolbar>
-    </StackItem>
-    <StackItem>
-      <Section type="content" id="users">
-        <UsersList
-          userLinks={activeUserPermissions.userAccessAdministrator || activeUserPermissions.orgAdmin}
-          props={ {
-            isSelectable: false,
-            isCompact: false
-          } }
-        />
-      </Section>
-    </StackItem>
-  </Stack >;
+  const renderUsers = () => (
+    <Stack>
+      <StackItem>
+        <TopToolbar>
+          <TopToolbarTitle title={intl.formatMessage(messages.users)} description={description} />
+        </TopToolbar>
+      </StackItem>
+      <StackItem>
+        <Section type="content" id="users">
+          <UsersList
+            userLinks={activeUserPermissions.userAccessAdministrator || activeUserPermissions.orgAdmin}
+            props={{
+              isSelectable: false,
+              isCompact: false,
+            }}
+          />
+        </Section>
+      </StackItem>
+    </Stack>
+  );
 
   return (
     <Switch>
-      <PageActionRoute pageAction="user-detail"  path={ paths['user-detail'].path } render={ props => <User {...props}/> } />
-      <PageActionRoute pageAction="users-list" path={ [ paths.users.path, paths.rbac.path] } render={ () => renderUsers() } />
+      <PageActionRoute pageAction="user-detail" path={paths['user-detail'].path} render={(props) => <User {...props} />} />
+      <PageActionRoute pageAction="users-list" path={[paths.users.path, paths.rbac.path]} render={() => renderUsers()} />
     </Switch>
-
   );
-};
-
-Users.propTypes = {
-
-};
-
-Users.defaultProps = {
-
 };
 
 export default Users;
