7cbc18d0836e1c2c80e88e95dab65aff0a4d88ef
Filip Hlavac <filip.hlavac01@gmail.com>
2023-06-14 16:25:19 +0200

    Use PF component-groups NotAuthorized component

--- src/presentational-components/shared/ErrorReducerCatcher.js
+++ src/presentational-components/shared/ErrorReducerCatcher.js
@@ -1,15 +1,32 @@
 import React, { useEffect, Fragment } from 'react';
-import { useIntl } from 'react-intl';
+import { FormattedMessage, useIntl } from 'react-intl';
 import PropTypes from 'prop-types';
-import messages from '../../Messages';
 import { useDispatch, useSelector } from 'react-redux';
-import NotAuthorized from '@redhat-cloud-services/frontend-components/NotAuthorized/NotAuthorized';
+import { NotAuthorized } from '@patternfly/react-component-groups';
+
 import { useLocation } from 'react-router-dom';
 import { API_ERROR } from '../../redux/action-types';
 import NotificationPortal from '@redhat-cloud-services/frontend-components-notifications/NotificationPortal/';
+import messages from '../../Messages';
 
 const errorStates = {
-  403: NotAuthorized,
+  403: (props) => (
+    <NotAuthorized
+      {...props}
+      description={
+        <FormattedMessage
+          {...messages.notAuthorized}
+          values={{
+            page: (
+              <>
+                &nbsp;<a href={`./iam/my-user-access`}>My User Access</a>&nbsp;
+              </>
+            ),
+          }}
+        />
+      }
+    />
+  ),
 };
 
 const ErroReducerCatcher = ({ children }) => {
