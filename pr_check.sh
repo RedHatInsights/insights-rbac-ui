1629dc5971fcb82c535fa9eadc2bcd467d6f1094
Petr Bartos <mrpetrbartos@gmail.com>
2023-06-16 17:40:32 +0200

    Run IQE UI tests as part of PR check

--- pr_check.sh
+++ pr_check.sh
@@ -11,17 +11,46 @@ export WORKSPACE=${WORKSPACE:-$APP_ROOT} # if running in jenkins, use the build'
 export APP_ROOT=$(pwd)
 COMMON_BUILDER=https://raw.githubusercontent.com/RedHatInsights/insights-frontend-builder-common/master
 
-# --------------------------------------------
-# Options that must be configured by app owner
-# --------------------------------------------
-IQE_PLUGINS="rbac"
-IQE_MARKER_EXPRESSION="smoke"
-IQE_FILTER_EXPRESSION=""
-
 set -exv
 # source is preferred to | bash -s in this case to avoid a subshell
 source <(curl -sSL $COMMON_BUILDER/src/frontend-build.sh)
 BUILD_RESULTS=$?
+
+# Ensure that we deploy the right component for testing
+export COMPONENT="rbac"
+export APP_NAME=rbac
+export COMPONENT_NAME="rbac"
+
+# Install bonfire
+CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
+curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh
+
+source .cicd_bootstrap.sh
+
+echo "Taking a short nap"
+sleep 60
+
+
+set -x
+# Deploy to an ephemeral namespace for testing
+export IMAGE="quay.io/cloudservices/rbac"
+export GIT_COMMIT=master
+export IMAGE_TAG=latest
+export DEPLOY_FRONTENDS=true
+source $CICD_ROOT/deploy_ephemeral_env.sh
+
+# Run some tests with ClowdJobInvocation
+export IQE_IMAGE_TAG="rbac"
+IQE_PLUGINS="rbac_frontend"
+IQE_MARKER_EXPRESSION="smoke"
+# Exclude tests
+IQE_FILTER_EXPRESSION=""
+IQE_ENV="ephemeral"
+IQE_SELENIUM="true"
+IQE_CJI_TIMEOUT="30m"
+DEPLOY_TIMEOUT="900"  # 15min
+DEPLOY_FRONTENDS="true"
+source $CICD_ROOT/cji_smoke_test.sh
 
 # Stubbed out for now
 mkdir -p $WORKSPACE/artifacts
