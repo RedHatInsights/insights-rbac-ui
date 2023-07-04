200f0cd80ffcd1ce147751364ba4dc30ebda9d39
Viktoriia Doshchenko <doshchenko.victoria@gmail.com>
2023-07-04 10:29:36 +0200

    Create new component MainTable

--- src/presentational-components/shared/table-composable-toolbar-view.tsx
+++ src/presentational-components/shared/table-composable-toolbar-view.tsx
@@ -93,8 +93,192 @@ interface TableProps {
   noDataDescription?: Array<React.ReactNode>;
   ouiaId: string;
   tableId?: string;
-  textFilterRef?: undefined;
+  textFilterRef?: undefined; // TODO: check the usage
 }
+
+interface MainTableProps {
+  columns: Array<{ title: string; key?: string; sortable?: boolean }>;
+  isSelectable: boolean;
+  isLoading: boolean;
+  noData?: boolean;
+  data?: Array<unknown>; // used only in toolbar for selectable items
+  title: { singular: string; plural: string };
+  filterValue?: string;
+  setFilterValue: (value: FilterProps) => void;
+  pagination: { limit?: number; offset?: number; count?: number; noBottom?: boolean };
+  fetchData: (config: FetchDataProps) => void;
+  toolbarButtons?: () => Array<unknown>;
+  filterPlaceholder?: string;
+  filters: Array<{
+    value: string | number | Array<unknown>;
+    key: string;
+    placeholder?: string;
+    innerRef?: React.RefObject<HTMLInputElement | HTMLTextAreaElement>;
+    label?: string;
+    type?: any;
+    items?: any;
+  }>;
+  isFilterable?: boolean;
+  onShowMore?: () => void;
+  showMoreTitle?: string;
+  onFilter?: () => void;
+  onChange?: () => void;
+  value?: unknown;
+  hideFilterChips?: boolean;
+  tableId?: string;
+  textFilterRef?: undefined;
+  rows: Array<RowProps>;
+  sortBy: ISortBy;
+  onSort: OnSort;
+  isCompact: boolean;
+  borders: boolean;
+  ouiaId: string;
+  noDataDescription?: Array<React.ReactNode>;
+  emptyFilters: FilterProps;
+}
+
+const MainTable = ({
+  // props for toolbar
+  columns,
+  isSelectable,
+  isLoading,
+  noData,
+  data,
+  title,
+  filterValue,
+  setFilterValue,
+  pagination,
+  fetchData,
+  toolbarButtons,
+  filterPlaceholder,
+  filters,
+  isFilterable,
+  onShowMore,
+  showMoreTitle,
+  onFilter,
+  onChange,
+  value,
+  hideFilterChips,
+  tableId,
+  textFilterRef,
+  //
+  rows,
+  sortBy,
+  onSort,
+  isCompact,
+  borders,
+  ouiaId,
+  noDataDescription,
+  emptyFilters,
+}: MainTableProps) => {
+  const orderBy = sortBy?.index ? `${sortBy?.direction === 'desc' ? '-' : ''}${columns[sortBy.index]?.key}` : undefined;
+  const intl = useIntl();
+  return (
+    <Fragment>
+      <Toolbar
+        isSelectable={isSelectable}
+        isLoading={isLoading || noData}
+        data={data}
+        titleSingular={title.singular}
+        filterValue={filterValue}
+        setFilterValue={setFilterValue}
+        sortBy={orderBy}
+        pagination={pagination}
+        fetchData={fetchData}
+        toolbarButtons={toolbarButtons}
+        filterPlaceholder={filterPlaceholder}
+        filters={filters}
+        isFilterable={isFilterable}
+        onShowMore={onShowMore}
+        showMoreTitle={showMoreTitle}
+        onFilter={onFilter}
+        onChange={onChange}
+        value={value}
+        hideFilterChips={hideFilterChips}
+        tableId={tableId}
+        textFilterRef={textFilterRef}
+      />
+      {isLoading ? (
+        <ListLoader isCompact={isCompact} items={pagination?.limit} />
+      ) : (
+        <TableComposable
+          aria-label={`${title.plural.toLowerCase()} table`}
+          variant={isCompact ? TableVariant.compact : undefined}
+          borders={borders}
+          ouiaId={ouiaId} // [PF]: Value to overwrite the randomly generated data-ouia-component-id
+        >
+          <Thead>
+            <Tr>
+              {columns.map((column, i) => (
+                <Th key={i} sort={column?.sortable ? { columnIndex: i, sortBy, onSort } : undefined}>
+                  {column.title}
+                </Th>
+              ))}
+            </Tr>
+          </Thead>
+          <Tbody>
+            {rows?.length > 0 ? (
+              rows?.map((row, i) => (
+                <Tr key={i}>
+                  {row.cells.map((cell, j) => (
+                    <Td key={j} dataLabel={columns[j].title}>
+                      {/* TODO: make more general */}
+                      {isCellObject(cell) ? cell.title : cell}
+                    </Td>
+                  ))}
+                </Tr>
+              ))
+            ) : (
+              <Tr>
+                {/* render one component full width for the entire row */}
+                <Td colSpan={columns.length}>
+                  <EmptyWithAction
+                    title={intl.formatMessage(messages.noMatchingItemsFound, { items: title.plural })}
+                    description={
+                      noData && noDataDescription
+                        ? noDataDescription
+                        : [
+                            intl.formatMessage(messages.filterMatchesNoItems, { items: title.plural }),
+                            intl.formatMessage(messages.tryChangingFilters),
+                          ]
+                    }
+                    actions={
+                      noData && noDataDescription
+                        ? undefined
+                        : [
+                            <EmptyStatePrimary key="clear-filters">
+                              <Button
+                                variant="link"
+                                ouiaId="clear-filters-button"
+                                onClick={() => {
+                                  setFilterValue(emptyFilters);
+                                  fetchData({
+                                    ...pagination,
+                                    offset: 0,
+                                    ...(emptyFilters ? emptyFilters : { name: '' }),
+                                  });
+                                }}
+                              >
+                                {intl.formatMessage(messages.clearAllFilters)}
+                              </Button>
+                            </EmptyStatePrimary>,
+                          ]
+                    }
+                  />
+                </Td>
+              </Tr>
+            )}
+          </Tbody>
+        </TableComposable>
+      )}
+      {!pagination.noBottom && (
+        <TableToolbar>
+          {!isLoading && <Pagination {...paginationBuilder(pagination, fetchData, filterValue, orderBy)} variant="bottom" dropDirection="up" />}
+        </TableToolbar>
+      )}
+    </Fragment>
+  );
+};
 
 export const TableComposableToolbarView = ({
   isCompact = false,
@@ -131,114 +315,6 @@ export const TableComposableToolbarView = ({
 }: TableProps) => {
   const intl = useIntl();
 
-  const renderEmpty = () => (
-    <EmptyWithAction
-      title={intl.formatMessage(messages.noMatchingItemsFound, { items: title.plural })}
-      description={
-        noData && noDataDescription
-          ? noDataDescription
-          : [intl.formatMessage(messages.filterMatchesNoItems, { items: title.plural }), intl.formatMessage(messages.tryChangingFilters)]
-      }
-      actions={
-        noData && noDataDescription
-          ? undefined
-          : [
-              <EmptyStatePrimary key="clear-filters">
-                <Button
-                  variant="link"
-                  ouiaId="clear-filters-button"
-                  onClick={() => {
-                    setFilterValue(emptyFilters);
-                    fetchData({
-                      ...pagination,
-                      offset: 0,
-                      ...(emptyFilters ? emptyFilters : { name: '' }),
-                    });
-                  }}
-                >
-                  {intl.formatMessage(messages.clearAllFilters)}
-                </Button>
-              </EmptyStatePrimary>,
-            ]
-      }
-    />
-  );
-
-  const renderTable = () => {
-    const orderBy = sortBy?.index ? `${sortBy?.direction === 'desc' ? '-' : ''}${columns[sortBy.index]?.key}` : undefined;
-    return (
-      <Fragment>
-        <Toolbar
-          isSelectable={isSelectable}
-          isLoading={isLoading || noData}
-          data={data}
-          titleSingular={title.singular}
-          filterValue={filterValue}
-          setFilterValue={setFilterValue}
-          sortBy={orderBy}
-          pagination={pagination}
-          fetchData={fetchData}
-          toolbarButtons={toolbarButtons}
-          filterPlaceholder={filterPlaceholder}
-          filters={filters}
-          isFilterable={isFilterable}
-          onShowMore={onShowMore}
-          showMoreTitle={showMoreTitle}
-          onFilter={onFilter}
-          onChange={onChange}
-          value={value}
-          hideFilterChips={hideFilterChips}
-          tableId={tableId}
-          textFilterRef={textFilterRef}
-        />
-        {isLoading ? (
-          <ListLoader isCompact={isCompact} items={pagination?.limit} />
-        ) : (
-          <TableComposable
-            aria-label={`${title.plural.toLowerCase()} table`}
-            variant={isCompact ? TableVariant.compact : undefined}
-            borders={borders}
-            ouiaId={ouiaId} // [PF]: Value to overwrite the randomly generated data-ouia-component-id
-          >
-            <Thead>
-              <Tr>
-                {columns.map((column, i) => (
-                  <Th key={i} sort={column?.sortable ? { columnIndex: i, sortBy, onSort } : undefined}>
-                    {column.title}
-                  </Th>
-                ))}
-              </Tr>
-            </Thead>
-            <Tbody>
-              {rows?.length > 0 ? (
-                rows?.map((row, i) => (
-                  <Tr key={i}>
-                    {row.cells.map((cell, j) => (
-                      <Td key={j} dataLabel={columns[j].title}>
-                        {/* TODO: make more general */}
-                        {isCellObject(cell) ? cell.title : cell}
-                      </Td>
-                    ))}
-                  </Tr>
-                ))
-              ) : (
-                <Tr>
-                  {/* render one component full width for the entire row */}
-                  <Td colSpan={columns.length}>{renderEmpty()}</Td>
-                </Tr>
-              )}
-            </Tbody>
-          </TableComposable>
-        )}
-        {!pagination.noBottom && (
-          <TableToolbar>
-            {!isLoading && <Pagination {...paginationBuilder(pagination, fetchData, filterValue, orderBy)} variant="bottom" dropDirection="up" />}
-          </TableToolbar>
-        )}
-      </Fragment>
-    );
-  };
-
   return (
     <Fragment>
       {!isLoading && rows?.length === 0 && filterValue?.length === 0 && filters.every(({ value }) => !value) ? (
@@ -253,7 +329,38 @@ export const TableComposableToolbarView = ({
           {...(typeof emptyProps === 'object' ? emptyProps : {})}
         />
       ) : (
-        renderTable()
+        <MainTable
+          columns={columns}
+          isSelectable={isSelectable}
+          isLoading={isLoading}
+          noData={noData}
+          data={data}
+          title={title}
+          filterValue={filterValue}
+          setFilterValue={setFilterValue}
+          pagination={pagination}
+          fetchData={fetchData}
+          toolbarButtons={toolbarButtons}
+          filterPlaceholder={filterPlaceholder}
+          filters={filters}
+          isFilterable={isFilterable}
+          onShowMore={onShowMore}
+          showMoreTitle={showMoreTitle}
+          onFilter={onFilter}
+          onChange={onChange}
+          value={value}
+          hideFilterChips={hideFilterChips}
+          tableId={tableId}
+          textFilterRef={textFilterRef}
+          rows={rows}
+          sortBy={sortBy}
+          onSort={onSort}
+          isCompact={isCompact}
+          borders={borders}
+          ouiaId={ouiaId}
+          noDataDescription={noDataDescription}
+          emptyFilters={emptyFilters}
+        />
       )}
     </Fragment>
   );
