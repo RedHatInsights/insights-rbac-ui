c5635b4af27ccd59d8f03eec50dafb04d0c341fe
Alexandra Ferdinand <aferdina@redhat.com>
2023-07-20 13:19:49 -0400

    intial commit

--- src/smart-components/group/group-table-helpers.js
+++ src/smart-components/group/group-table-helpers.js
@@ -9,6 +9,8 @@ import AppLink from '../../presentational-components/shared/AppLink';
 import { getDateFormat } from '../../helpers/shared/helpers';
 import pathnames from '../../utilities/pathnames';
 import messages from '../../Messages';
+import { Text } from '@patternfly/react-core';
+import { Table, TableBody, TableHeader, TableVariant } from '@patternfly/react-table';
 
 const DefaultPlatformPopover = ({ id, uuid, bodyContent }) => {
   const [isPopoverVisible, setPopoverVisible] = useState(false);
@@ -40,48 +42,144 @@ DefaultPlatformPopover.propTypes = {
   bodyContent: PropTypes.string.isRequired,
 };
 
-export const createRows = (isAdmin, data, selectedRows = []) => {
+export const createRows = (isAdmin, data, selectedRows, expanded = []) => {
   const intl = useIntl();
-  return data.reduce(
-    (acc, { uuid, name, roleCount, principalCount, modified, platform_default: isPlatformDefault, admin_default: isAdminDefault }) => [
+  return (
+    data.reduce((acc, { uuid, name, roleCount, principalCount, modified, platform_default: isPlatformDefault, admin_default: isAdminDefault }) => [
       ...acc,
       {
         uuid,
         isAdminDefault,
         isPlatformDefault,
-        cells: [
-          <Fragment key={uuid}>
-            <div className="pf-m-inline-flex">
-              {isAdmin ? (
-                <AppLink
-                  key={`${uuid}-link`}
-                  state={{ uuid }}
-                  to={pathnames['group-detail'].link.replace(':groupId', isPlatformDefault ? 'default-access' : uuid)}
-                >
-                  {name}
-                </AppLink>
-              ) : (
-                name
-              )}
-              {(isPlatformDefault || isAdminDefault) && (
-                <DefaultPlatformPopover
-                  id={`default${isAdminDefault ? '-admin' : ''}-group-popover`}
-                  uuid={uuid}
-                  key={`${uuid}-popover`}
-                  bodyContent={intl.formatMessage(isAdminDefault ? messages.orgAdminInheritedRoles : messages.usersInheritedRoles)}
-                />
-              )}
-            </div>
-          </Fragment>,
-          roleCount,
-          principalCount,
-          <Fragment key={`${uuid}-modified`}>
-            <DateFormat date={modified} type={getDateFormat(modified)} />
-          </Fragment>,
-        ],
         selected: Boolean(selectedRows && selectedRows.find((row) => row.uuid === uuid)),
+        cells: [
+          {
+            title: (
+              <AppLink
+                key={`${uuid}-link`}
+                state={{ uuid }}
+                to={pathnames['group-detail'].link.replace(':groupId', isPlatformDefault ? 'default-access' : uuid)}
+              >
+                {name}
+              </AppLink>
+            ),
+          },
+          { title: roleCount, props: { isOpen: expanded[uuid] === 2 } },
+          { title: principalCount, props: { isOpen: expanded[uuid] === 3 } },
+          { title: <DateFormat date={modified} type={getDateFormat(modified)} /> },
+        ],
       },
-    ],
+      {
+        uuid: `${uuid}-roles`,
+        parent: 2 * 1,
+        compoundParent: 1,
+        fullWidth: true,
+        noPadding: true,
+        cells: [
+          {
+            props: { colSpan: 7, className: 'pf-m-no-padding' },
+            title:
+              roleCount.length > 0 ? (
+                <Table
+                  ouiaId="roles-in-group-nested-table"
+                  aria-label="Simple Table"
+                  variant={TableVariant.compact}
+                  cells={[
+                    intl.formatMessage(messages.roleName),
+                    intl.formatMessage(messages.description),
+                    intl.formatMessage(messages.permissions),
+                    intl.formatMessage(messages.lastModified),
+                  ]}
+                  rows={roleCount.map((roles) => {
+                    const [roleName, description, permissions] = roles.permission.split(':');
+                    return {
+                      cells: [
+                        roleName,
+                        description,
+                        permissions,
+                        <Fragment key={`${uuid}-modified`}>
+                          <DateFormat date={modified} type={getDateFormat(modified)} />
+                        </Fragment>,
+                      ],
+                    };
+                  })}
+                >
+                  <TableHeader />
+                  <TableBody />
+                </Table>
+              ) : (
+                <Text className="pf-u-mx-lg pf-u-my-sm">{intl.formatMessage(messages.noRolePermissions)}</Text>
+              ),
+          },
+        ],
+      },
+      {
+        uuid: `${uuid}-members`,
+        parent: 3 * 1,
+        compoundParent: 2,
+        fullwidth: true,
+        noPadding: true,
+        cells: [
+          {
+            props: { colSpan: 7, className: 'pf-m-no-padding' },
+            title:
+              principalCount > 0 ? (
+                <Table
+                  aria-label="Simple Table"
+                  ouiaId="members-in-group-nested-table"
+                  variant={TableVariant.compact}
+                  cells={[
+                    intl.formatMessage(messages.orgAdmin),
+                    intl.formatMessage(messages.username),
+                    intl.formatMessage(messages.email),
+                    intl.formatMessage(messages.firstName),
+                    intl.formatMessage(messages.lastName),
+                    intl.formatMessage(messages.status),
+                  ]}
+                  rows={principalCount.map((member) => ({
+                    cells: [member.orgAdmin, member.username, member.email, member.firstName, member.lastName, member.status],
+                  }))}
+                >
+                  <TableHeader />
+                  <TableBody />
+                </Table>
+              ) : (
+                <Text className="pf-u-mx-lg pf-u-my-sm">{intl.formatMessage(messages.noGroupMembers)}</Text>
+              ),
+          },
+        ],
+      },
+    ]),
     []
   );
 };
+// cells: [
+//   <Fragment key={uuid}>
+//     <div className="pf-m-inline-flex">
+//       {isAdmin ? (
+//         <AppLink
+//           key={`${uuid}-link`}
+//           state={{ uuid }}
+//           to={pathnames['group-detail'].link.replace(':groupId', isPlatformDefault ? 'default-access' : uuid)}
+//         >
+//           {name}
+//         </AppLink>
+//       ) : (
+//         name
+//       )}
+//       {(isPlatformDefault || isAdminDefault) && (
+//         <DefaultPlatformPopover
+//           id={`default${isAdminDefault ? '-admin' : ''}-group-popover`}
+//           uuid={uuid}
+//           key={`${uuid}-popover`}
+//           bodyContent={intl.formatMessage(isAdminDefault ? messages.orgAdminInheritedRoles : messages.usersInheritedRoles)}
+//         />
+//       )}
+//     </div>
+//   </Fragment>,
+//   roleCount,
+//   principalCount,
+//   <Fragment key={`${uuid}-modified`}>
+//     <DateFormat date={modified} type={getDateFormat(modified)} />
+//   </Fragment>,
+// ],
