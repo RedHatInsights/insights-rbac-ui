a512188c458d8e209c0a65a13de1ed782c32d11c
Alexandra Ferdinand <aferdina@redhat.com>
2023-10-24 06:46:06 -0400

    .

--- src/smart-components/group/group-table-helpers.js
+++ src/smart-components/group/group-table-helpers.js
@@ -56,6 +56,7 @@ export const createRows = (isAdmin, data, selectedRows, expanded = []) => {
         uuid,
         isAdminDefault,
         isPlatformDefault,
+        selected: Boolean(selectedRows && selectedRows.find((row) => row.uuid === uuid)),
         cells: [
           {
             title: isAdmin ? (
@@ -153,7 +154,6 @@ export const createRows = (isAdmin, data, selectedRows, expanded = []) => {
               ),
           },
         ],
-        selected: Boolean(selectedRows && selectedRows.find((row) => row.uuid === uuid)),
       },
     ],
     []
