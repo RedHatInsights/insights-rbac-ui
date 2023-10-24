abb02da3eba80c71f053a0a3397292017368f495
Alexandra Ferdinand <aferdina@redhat.com>
2023-10-24 08:04:21 -0400

    commit latest changes

--- src/smart-components/group/groups.js
+++ src/smart-components/group/groups.js
@@ -9,7 +9,7 @@ import Section from '@redhat-cloud-services/frontend-components/Section';
 import { shallowEqual, useSelector, useDispatch } from 'react-redux';
 import { TableToolbarView } from '../../presentational-components/shared/table-toolbar-view';
 import { createRows } from './group-table-helpers';
-import { fetchAdminGroup, fetchGroups, fetchSystemGroup } from '../../redux/actions/group-actions';
+import { fetchAdminGroup, fetchGroups, fetchRolesForGroup, fetchSystemGroup } from '../../redux/actions/group-actions';
 import AppLink, { mergeToBasename } from '../../presentational-components/shared/AppLink';
 import { TopToolbar, TopToolbarTitle } from '../../presentational-components/shared/top-toolbar';
 import GroupRowWrapper from './group-row-wrapper';
@@ -183,8 +183,16 @@ const Groups = () => {
     group.platform_default || group.admin_default ? { ...group, principalCount: `All${group.admin_default ? ' org admins' : ''}` } : group
   );
 
-  const onExpand = (_event, _rowIndex, colIndex, isOpen, rowData) =>
-    setExpanded({ ...expanded, [rowData.uuid]: isOpen ? -1 : colIndex + Number(!isAdmin) });
+  const fetchExpandTableDetailsForGroup = (uuid) => dispatch(fetchRolesForGroup(uuid));
+
+  const onExpand = (_event, _rowIndex, colIndex, isOpen, rowData) => {
+    if (!isOpen) {
+      setExpanded({ ...expanded, [rowData.uuid]: colIndex + Number(!isAdmin) });
+      colIndex + Number(!isAdmin) === 2 && fetchExpandTableDetailsForGroup(rowData.uuid);
+    } else {
+      setExpanded({ ...expanded, [rowData.uuid]: -1 });
+    }
+  };
 
   const rows = createRows(isAdmin, data, selectedRows, pagination, expanded);
   // used for (not) reseting the filters after submit
