9a4f029c401c4132d10fb9f6ce7ff445485b150b
Radek Kalužík <radek.kaluzik@email.cz>
2023-11-30 11:40:34 +0100

    Initial Cypress config for RBAC

--- pr_check.sh
+++ pr_check.sh
@@ -19,6 +19,19 @@ IQE_MARKER_EXPRESSION="smoke"
 IQE_FILTER_EXPRESSION=""
 
 set -exv
+
+docker run -t \
+  -v $PWD:/e2e:ro,Z \
+  -w /e2e \
+  -e CHROME_ACCOUNT=$CHROME_ACCOUNT \
+  -e CHROME_PASSWORD=$CHROME_PASSWORD \
+  --add-host stage.foo.redhat.com:127.0.0.1 \
+  --add-host prod.foo.redhat.com:127.0.0.1 \
+  --entrypoint bash \
+  quay.io/cloudservices/cypress-e2e-image:971bc23 /e2e/run-e2e.sh
+
+echo "After docker run"
+
 # source is preferred to | bash -s in this case to avoid a subshell
 source <(curl -sSL $COMMON_BUILDER/src/frontend-build.sh)
 BUILD_RESULTS=$?
