24e7e201fcc017eab31455818ebc70fa1855e178
Filip Hlavac <filip.hlavac01@gmail.com>
2024-02-04 22:39:10 +0100

    Make CG WarningModal used in delete group

--- src/smart-components/group/remove-group-modal.js
+++ src/smart-components/group/remove-group-modal.js
@@ -1,15 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useParams } from 'react-router-dom';
 import { FormattedMessage, useIntl } from 'react-intl';
 import { shallowEqual, useDispatch, useSelector } from 'react-redux';
-import { Button, Checkbox, Modal, ModalVariant, Text, TextContent } from '@patternfly/react-core';
-import { ExclamationTriangleIcon } from '@patternfly/react-icons';
+import { ButtonVariant, Text, TextContent } from '@patternfly/react-core';
+import WarningModal from '@patternfly/react-component-groups/dist/dynamic/WarningModal';
 import { fetchGroup, removeGroups } from '../../redux/actions/group-actions';
 import { FormItemLoader } from '../../presentational-components/shared/loader-placeholders';
 import useAppNavigate from '../../hooks/useAppNavigate';
 import messages from '../../Messages';
-import './remove-group-modal.scss';
 
 const RemoveGroupModal = ({ postMethod, pagination, cancelRoute, submitRoute = cancelRoute }) => {
   const intl = useIntl();
@@ -31,8 +30,6 @@ const RemoveGroupModal = ({ postMethod, pagination, cancelRoute, submitRoute = c
     groupsToRemove.length === 1 && dispatch(fetchGroup(groupsToRemove[0]));
   }, []);
 
-  const [checked, setChecked] = useState(false);
-
   const onSubmit = () => {
     dispatch(removeGroups(groupsToRemove))
       .then(() => postMethod(groupsToRemove, { limit: pagination?.limit }))
@@ -42,25 +39,15 @@ const RemoveGroupModal = ({ postMethod, pagination, cancelRoute, submitRoute = c
   const onCancel = () => navigate(cancelRoute);
 
   return (
-    <Modal
-      className="rbac"
+    <WarningModal
       isOpen
-      variant={ModalVariant.small}
-      title={
-        <Text>
-          <ExclamationTriangleIcon className="delete-group-warning-icon" />
-          &nbsp; {intl.formatMessage(multipleGroups ? messages.deleteGroupsQuestion : messages.deleteGroupQuestion)}
-        </Text>
-      }
+      withCheckbox
+      title={intl.formatMessage(multipleGroups ? messages.deleteGroupsQuestion : messages.deleteGroupQuestion)}
+      checkboxLabel={intl.formatMessage(messages.understandActionIrreversible)}
+      confirmButtonLabel={intl.formatMessage(multipleGroups ? messages.deleteGroups : messages.deleteGroup)}
+      confirmButtonVariant={ButtonVariant.danger}
       onClose={onCancel}
-      actions={[
-        <Button key="submit" isDisabled={!checked} variant="danger" type="button" onClick={onSubmit}>
-          {intl.formatMessage(multipleGroups ? messages.deleteGroups : messages.deleteGroup)}
-        </Button>,
-        <Button key="cancel" variant="link" type="button" onClick={onCancel}>
-          {intl.formatMessage(messages.cancel)}
-        </Button>,
-      ]}
+      onConfirm={onSubmit}
     >
       <TextContent>
         {multipleGroups ? (
@@ -87,14 +74,7 @@ const RemoveGroupModal = ({ postMethod, pagination, cancelRoute, submitRoute = c
           </Text>
         )}
       </TextContent>
-      &nbsp;
-      <Checkbox
-        isChecked={checked}
-        onChange={() => setChecked(!checked)}
-        label={intl.formatMessage(messages.understandActionIrreversible)}
-        id="delete-group-check"
-      />
-    </Modal>
+    </WarningModal>
   );
 };
 
@@ -103,7 +83,7 @@ RemoveGroupModal.propTypes = {
   pagination: PropTypes.shape({
     limit: PropTypes.number.isRequired,
   }).isRequired,
-  filters: PropTypes.object.isRequired,
+  filters: PropTypes.object,
   cancelRoute: PropTypes.oneOfType([
     PropTypes.string,
     PropTypes.shape({
