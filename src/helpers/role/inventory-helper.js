7eb5b4d1b8ed1b3dd5a3dff5f8352cdc3da02d10
Alexandra Ferdinand <aferdina@redhat.com>
2024-05-03 11:24:30 -0400

    update snapshot

--- src/helpers/role/inventory-helper.js
+++ src/helpers/role/inventory-helper.js
@@ -1,11 +1,12 @@
 import { getInventoryGroupsApi } from '../shared/user-login';
 import { useFlag } from '@unleash/proxy-client-react';
-import useChrome from '@redhat-cloud-services/frontend-components/useChrome';
+import { useChrome } from '@redhat-cloud-services/frontend-components/useChrome';
 
 const inventoryGroupsApi = getInventoryGroupsApi();
+const { isBeta } = useChrome();
 
 export const getInventoryGroups = async (apiProps) => {
   return await inventoryGroupsApi.getInventoryGroups(apiProps);
 };
 
-export const enableWorkspacesNameChange = useChrome.isBeta() && useFlag('platform.rbac.groups-to-workspaces-rename');
+export const enableWorkspacesNameChange = isBeta && useFlag('platform.rbac.groups-to-workspaces-rename');
