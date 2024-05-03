858ddf6da89297d6cf8cf28b6e2e5e27a43a97c6
Alexandra Ferdinand <aferdina@redhat.com>
2024-05-03 11:51:08 -0400

    updated feature flag so no need for isBeta

--- src/helpers/role/inventory-helper.js
+++ src/helpers/role/inventory-helper.js
@@ -1,17 +1,15 @@
 import { useFlag } from '@unleash/proxy-client-react';
-import { useChrome } from '@redhat-cloud-services/frontend-components/useChrome';
 import flatten from 'lodash/flatten';
 import { getInventoryGroupsApi, getInventoryResourceTypesApi } from '../shared/user-login';
 
 const inventoryResourceTypesApi = getInventoryResourceTypesApi();
 const inventoryGroupsApi = getInventoryGroupsApi();
-const { isBeta } = useChrome();
 
 export const getInventoryGroups = async ({ name, perPage, page, options } = {}) => {
   return await inventoryResourceTypesApi.apiResourceTypeGetResourceTypeGroupsList(name, perPage, page, options);
 };
 
-export const enableWorkspacesNameChange = isBeta && useFlag('platform.rbac.groups-to-workspaces-rename');
+export const enableWorkspacesNameChange = useFlag('platform.rbac.groups-to-workspaces-rename');
 
 export const getInventoryGroupsDetails = async (groupsIds) => {
   return await inventoryGroupsApi.apiGroupGetGroupsById(groupsIds);
