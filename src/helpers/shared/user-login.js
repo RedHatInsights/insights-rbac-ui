2bd3c408421b59c25e1fc7a1f7c239a62b0bc1eb
Karel Hala <khala@redhat.com>
2024-06-17 14:39:05 +0200

    Use v2 RBAC client to utilize treeshaking

--- src/helpers/shared/user-login.js
+++ src/helpers/shared/user-login.js
@@ -8,7 +8,40 @@ import {
   interceptor500,
   errorInterceptor,
 } from '@redhat-cloud-services/frontend-components-utilities/interceptors';
-import { GroupApi, PrincipalApi, RoleApi, PolicyApi, AccessApi, PermissionApi } from '@redhat-cloud-services/rbac-client';
+
+import listPrincipals from '@redhat-cloud-services/rbac-client/dist/ListPrincipals';
+
+import listGroups from '@redhat-cloud-services/rbac-client/dist/ListGroups';
+import listRoles from '@redhat-cloud-services/rbac-client/dist/ListRoles';
+import getGroup from '@redhat-cloud-services/rbac-client/dist/GetGroup';
+import updateGroup from '@redhat-cloud-services/rbac-client/dist/UpdateGroup';
+import createGroup from '@redhat-cloud-services/rbac-client/dist/CreateGroup';
+import addPrincipalToGroup from '@redhat-cloud-services/rbac-client/dist/AddPrincipalToGroup';
+import addRoleToGroup from '@redhat-cloud-services/rbac-client/dist/AddRoleToGroup';
+import deleteGroup from '@redhat-cloud-services/rbac-client/dist/DeleteGroup';
+import deletePrincipalFromGroup from '@redhat-cloud-services/rbac-client/dist/DeletePrincipalFromGroup';
+import getPrincipalsFromGroup from '@redhat-cloud-services/rbac-client/dist/GetPrincipalsFromGroup';
+import deleteRoleFromGroup from '@redhat-cloud-services/rbac-client/dist/DeleteRoleFromGroup';
+import listRolesForGroup from '@redhat-cloud-services/rbac-client/dist/ListRolesForGroup';
+
+import createRole from '@redhat-cloud-services/rbac-client/dist/CreateRole';
+import getRole from '@redhat-cloud-services/rbac-client/dist/GetRole';
+import deleteRole from '@redhat-cloud-services/rbac-client/dist/DeleteRole';
+import getRoleAccess from '@redhat-cloud-services/rbac-client/dist/GetRoleAccess';
+import updateRole from '@redhat-cloud-services/rbac-client/dist/UpdateRole';
+import patchRole from '@redhat-cloud-services/rbac-client/dist/PatchRole';
+
+import listPolicies from '@redhat-cloud-services/rbac-client/dist/ListPolicies';
+import getPolicy from '@redhat-cloud-services/rbac-client/dist/GetPolicy';
+import createPolicies from '@redhat-cloud-services/rbac-client/dist/CreatePolicies';
+import updatePolicy from '@redhat-cloud-services/rbac-client/dist/UpdatePolicy';
+import deletePolicy from '@redhat-cloud-services/rbac-client/dist/DeletePolicy';
+
+import getPrincipalAccess from '@redhat-cloud-services/rbac-client/dist/GetPrincipalAccess';
+import listPermissions from '@redhat-cloud-services/rbac-client/dist/ListPermissions';
+import listPermissionOptions from '@redhat-cloud-services/rbac-client/dist/ListPermissionOptions';
+
+import { APIFactory } from '@redhat-cloud-services/javascript-clients-shared';
 import { ResourceTypesApi, GroupsApi } from '@redhat-cloud-services/host-inventory-client';
 import { BaseAPI } from '@redhat-cloud-services/rbac-client/dist/base';
 
@@ -35,12 +68,73 @@ axiosInstance.interceptors.response.use(null, interceptor403);
 axiosInstance.interceptors.response.use(null, interceptor500);
 axiosInstance.interceptors.response.use(null, errorInterceptor);
 
-const principalApi = new PrincipalApi(undefined, RBAC_API_BASE, axiosInstance);
-const groupApi = new GroupApi(undefined, RBAC_API_BASE, axiosInstance);
-const roleApi = new RoleApi(undefined, RBAC_API_BASE, axiosInstance);
-const policyApi = new PolicyApi(undefined, RBAC_API_BASE, axiosInstance);
-const accessApi = new AccessApi(undefined, RBAC_API_BASE, axiosInstance);
-const permissionApi = new PermissionApi(undefined, RBAC_API_BASE, axiosInstance);
+const principalApi = new APIFactory(
+  RBAC_API_BASE,
+  {
+    listPrincipals,
+  },
+  { axios: axiosInstance }
+);
+
+const groupApi = new APIFactory(
+  RBAC_API_BASE,
+  {
+    listGroups,
+    getGroup,
+    updateGroup,
+    createGroup,
+    addPrincipalToGroup,
+    addRoleToGroup,
+    deleteGroup,
+    deletePrincipalFromGroup,
+    listRoles,
+    getPrincipalsFromGroup,
+    deleteRoleFromGroup,
+    listRolesForGroup,
+  },
+  { axios: axiosInstance }
+);
+const roleApi = new APIFactory(
+  RBAC_API_BASE,
+  {
+    createRole,
+    listRoles,
+    getRole,
+    deleteRole,
+    getRoleAccess,
+    updateRole,
+    patchRole,
+  },
+  { axios: axiosInstance }
+);
+const policyApi = new APIFactory(
+  RBAC_API_BASE,
+  {
+    listPolicies,
+    getPolicy,
+    createPolicies,
+    updatePolicy,
+    deletePolicy,
+  },
+  { axios: axiosInstance }
+);
+const accessApi = new APIFactory(
+  RBAC_API_BASE,
+  {
+    getPrincipalAccess,
+    listPermissions,
+    listPermissionOptions,
+  },
+  { axios: axiosInstance }
+);
+const permissionApi = new APIFactory(
+  RBAC_API_BASE,
+  {
+    listPermissions,
+    listPermissionOptions,
+  },
+  { axios: axiosInstance }
+);
 const costApi = new BaseAPI(undefined, COST_API_BASE, axiosInstance);
 const inventoryResourceTypesApi = new ResourceTypesApi(undefined, INVENTORY_API_BASE, axiosInstance);
 const inventoryGroupsApi = new GroupsApi(undefined, INVENTORY_API_BASE, axiosInstance);
