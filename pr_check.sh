88de96f316141ed5a456cc91093358422c61f41c
Karel Hala <khala@redhat.com>
2024-09-04 14:59:56 +0200

    Pull only active chrome quay tags (#1659)

--- pr_check.sh
+++ pr_check.sh
@@ -20,7 +20,7 @@ IQE_FILTER_EXPRESSION=""
 
 set -exv
 
-CHROME_SHA=$(curl -X GET "https://quay.io/api/v1/repository/cloudservices/insights-chrome-frontend/tag/" | jq .tags\[0\].name -r)
+CHROME_SHA=$(curl -X GET "https://quay.io/api/v1/repository/cloudservices/insights-chrome-frontend/tag/?onlyActiveTags=true" | jq .tags\[0\].name -r)
 CHROME_CONTAINER_NAME=chrome-$CHROME_SHA
 
 docker run -d --name $CHROME_CONTAINER_NAME --replace --network bridge quay.io/cloudservices/insights-chrome-frontend:$CHROME_SHA
