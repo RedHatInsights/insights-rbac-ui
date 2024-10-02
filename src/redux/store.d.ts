300510c8bcfc41214546f11c14b41b6842ac4b92
Cody Mitchell <comitche@redhat.com>
2024-10-02 03:42:07 -0400

    add basic user groups table

--- src/redux/store.d.ts
+++ src/redux/store.d.ts
@@ -1,7 +1,26 @@
+import { PaginationDefaultI } from '../helpers/shared/pagination';
 import { UserStore } from './reducers/user-reducer';
+
+interface GroupsStore {
+  groups: {
+    data: any[];
+    meta: any;
+    filters: any;
+    pagination: { count: number };
+  };
+  selectedGroup: {
+    addRoles: any;
+    members: { meta: any };
+    serviceAccounts: { meta: any };
+    pagination: PaginationDefaultI & { redirected?: boolean };
+  };
+  isLoading: boolean;
+  isRecordLoading: boolean;
+}
 
 export type RBACStore = {
   userReducer: UserStore;
+  groupsReducer: GroupsStore;
 };
 
 declare module 'react-redux' {
