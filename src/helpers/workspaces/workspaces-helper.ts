b36a4c8be450c864ae85f34bbc4257163c67c476
Filip Hlavac <50696716+fhlavac@users.noreply.github.com>
2024-12-10 15:15:32 +0100

    Add Create workspace wizard (#1715)
    * fix: fix User groups tab name
    
    * fix: improve Users and User groups loading states
    
    * feat: add translations for create workspace wizard
    
    * fix: fix UserGroups loading
    
    * feat: add create workspace wizard UI
    
    * feat: add basic create workspace wizart tests
    
    * fix: fix lint issues
    
    * feat: hook create workspace wizard to the API
    
    * feat: add helper texts per latest mockups
    
    * chore: update translations

--- src/helpers/workspaces/workspaces-helper.ts
+++ src/helpers/workspaces/workspaces-helper.ts
@@ -1,3 +1,4 @@
+import { WorkspaceCreateBody } from '../../redux/reducers/workspaces-reducer';
 import { getWorkspacesApi } from './api';
 
 const workspacesApi = getWorkspacesApi();
@@ -8,4 +9,8 @@ export async function getWorkspaces() {
 
 export async function getWorkspace(ws: string) {
   return await workspacesApi.getWorkspace({ id: ws });
+}
+
+export async function createWorkspace(config: WorkspaceCreateBody) {
+  return await workspacesApi.createWorkspace(config);
 }
