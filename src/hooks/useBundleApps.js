836d3f404767c22f7673ac8afdff26c68d768c5d
Austin Pinkerton <115492521+apinkert@users.noreply.github.com>
2025-01-14 05:19:02 -0500

    Add settings bundle and remove application_services for MyUserAccess (#1741)
    * feat(MyUserAccess): add settings bundle and remove application_services
    
    * test(MyUserAccess): add e2e test to verify Settings card

--- src/hooks/useBundleApps.js
+++ src/hooks/useBundleApps.js
@@ -5,7 +5,7 @@ import { DEFAULT_MUA_BUNDLE } from '../utilities/constants';
 const useBundleApps = (bundle) => {
   const { pathname } = useLocation();
   const navigate = useNavigate();
-  if (typeof bundle !== 'string' || bundle.length === 0 || !['application_services', 'openshift', 'rhel', 'ansible'].includes(bundle)) {
+  if (typeof bundle !== 'string' || bundle.length === 0 || !['openshift', 'rhel', 'ansible', 'settings'].includes(bundle)) {
     bundle = DEFAULT_MUA_BUNDLE;
     navigate({ to: pathname, search: `bundle=${bundle}` }, { replace: true });
     return [];
