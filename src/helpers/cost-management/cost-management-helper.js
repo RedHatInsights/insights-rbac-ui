4f6afcf45482597a8fdbe57c9a4a8cc2881db3d1
Riccardo Forina <rforina@redhat.com>
2025-05-16 12:25:28 +0200

    fix: handle empty responses from Akamai for the potentially optional cost API (#1834)

--- src/helpers/cost-management/cost-management-helper.js
+++ src/helpers/cost-management/cost-management-helper.js
@@ -1,9 +1,26 @@
 import { getCostApi } from '../shared/user-login';
 
+function isObjectResponse(response) {
+  return typeof response === 'object' && response !== null && !Array.isArray(response);
+}
+
 const costApi = getCostApi();
 
 export async function getResourceDefinitions(apiProps) {
-  return await costApi.getResourceTypes(apiProps);
+  try {
+    const res = await costApi.getResourceTypes(apiProps);
+    if (isObjectResponse(res)) {
+      return res;
+    }
+  } catch {
+    // this API can be not available on some environments
+  }
+
+  return {
+    data: [],
+    links: {},
+    meta: { count: 0, limit: 10, offset: 0 },
+  };
 }
 
 export async function getResource(apiProps) {
