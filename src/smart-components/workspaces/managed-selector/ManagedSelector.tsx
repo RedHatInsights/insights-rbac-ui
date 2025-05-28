e0591be7dd2918bd8fcede8c016c783fe0610f57
Cody Mitchell <comitche@redhat.com>
2025-05-28 15:13:06 -0400

    feat: export component for workspace selector

---
+++ src/smart-components/workspaces/managed-selector/ManagedSelector.tsx
@@ -0,0 +1,8 @@
+// basic empty component to be filled in later
+import React from 'react';
+
+const ManagedSelector = () => {
+  return <div>Managed Workspace Selector goes here</div>;
+}
+
+export default ManagedSelector;
