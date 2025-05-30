ce01ed5faaa182d47b440fed4d07fa7032c57dde
Cody Mitchell <comitche@redhat.com>
2025-05-30 10:24:58 -0400

    feat: create workspaces store

--- src/smart-components/workspaces/managed-selector/ManagedSelector.tsx
+++ src/smart-components/workspaces/managed-selector/ManagedSelector.tsx
@@ -1,8 +1,18 @@
-// basic empty component to be filled in later
 import React from 'react';
+import { useWorkspacesStore, WorkspacesStoreProvider } from './WorkspacesStore';
 
 const ManagedSelector = () => {
-  return <div>Managed Workspace Selector goes here</div>;
-}
+  const { store, isFetchingWorkspacesFromRBAC, setIsFetchingWorkspacesFromRBAC } = useWorkspacesStore();
+
+  return (
+    <WorkspacesStoreProvider>
+      <div>
+        <h1>Managed Selector</h1>
+        <p>{JSON.stringify(store)}</p>
+        <button onClick={() => setIsFetchingWorkspacesFromRBAC(!isFetchingWorkspacesFromRBAC)}>Toggle Fetching Workspaces</button>
+      </div>
+    </WorkspacesStoreProvider>
+  );
+};
 
 export default ManagedSelector;
---
+++ src/smart-components/workspaces/managed-selector/WorkspacesStore.tsx
@@ -0,0 +1,150 @@
+import React, { createContext, PropsWithChildren, useEffect, useMemo, useReducer } from 'react';
+import { TreeViewWorkspaceItem } from './TreeViewWorkspaceItem';
+
+interface WorkspacesStore {
+  isWorkspacesMenuExpanded: boolean;
+  isFetchingWorkspacesFromRBAC: boolean;
+  isFetchingWorkspacesFromRBACError: boolean;
+  selectedWorkspace?: TreeViewWorkspaceItem;
+  fetchedWorkspaces: TreeViewWorkspaceItem[];
+  workspaceTree?: TreeViewWorkspaceItem;
+}
+
+const initialStore: WorkspacesStore = {
+  isWorkspacesMenuExpanded: false,
+  isFetchingWorkspacesFromRBAC: false,
+  isFetchingWorkspacesFromRBACError: false,
+  selectedWorkspace: undefined,
+  fetchedWorkspaces: [],
+  workspaceTree: undefined,
+};
+
+const createWorkspacesStore = () => {
+  const store = { ...initialStore };
+
+  const subs = new Map<string, () => void>();
+
+  const notify = () => {
+    subs.forEach((sub) => sub());
+  };
+
+  const subscribe = (callback: () => void) => {
+    const id = crypto.randomUUID();
+    subs.set(id, callback);
+    return () => {
+      subs.delete(id);
+    };
+  };
+
+  const setIsWorkspacesMenuExpanded = (isExpanded: boolean) => {
+    store.isWorkspacesMenuExpanded = isExpanded;
+    notify();
+  };
+
+  const setIsFetchingWorkspacesFromRBAC = (isFetching: boolean) => {
+    store.isFetchingWorkspacesFromRBAC = isFetching;
+    notify();
+  };
+
+  const setIsFetchingWorkspacesFromRBACError = (isError: boolean) => {
+    store.isFetchingWorkspacesFromRBACError = isError;
+    notify();
+  };
+
+  const setSelectedWorkspace = (workspace: TreeViewWorkspaceItem | undefined) => {
+    store.selectedWorkspace = workspace;
+    notify();
+  };
+
+  const setFetchedWorkspaces = (workspaces: TreeViewWorkspaceItem[]) => {
+    store.fetchedWorkspaces = workspaces;
+    notify();
+  };
+
+  const setWorkspaceTree = (tree: any) => {
+    store.workspaceTree = tree;
+    notify();
+  };
+
+  return {
+    getStore: () => store,
+    subscribe,
+    setIsWorkspacesMenuExpanded,
+    setIsFetchingWorkspacesFromRBAC,
+    setIsFetchingWorkspacesFromRBACError,
+    setSelectedWorkspace,
+    setFetchedWorkspaces,
+    setWorkspaceTree,
+  };
+};
+
+const workspacesStore = createWorkspacesStore();
+
+const WorkspacesStoreContext = createContext(workspacesStore);
+
+const WorkspacesStoreProvider = (props: PropsWithChildren) => {
+  const store = useMemo(() => createWorkspacesStore(), []);
+  return <WorkspacesStoreContext.Provider value={store}>{props.children}</WorkspacesStoreContext.Provider>;
+};
+
+const useWorkspacesStore = () => {
+  const store = React.useContext(WorkspacesStoreContext);
+
+  if (!store) {
+    throw new Error('useWorkspacesStore must be used within a WorkspacesStoreProvider');
+  }
+
+  const [
+    {
+      isWorkspacesMenuExpanded,
+      isFetchingWorkspacesFromRBAC,
+      isFetchingWorkspacesFromRBACError,
+      selectedWorkspace,
+      fetchedWorkspaces,
+      workspaceTree,
+    },
+    dispatch,
+  ] = useReducer(
+    () => ({
+      isWorkspacesMenuExpanded: store.getStore().isWorkspacesMenuExpanded,
+      isFetchingWorkspacesFromRBAC: store.getStore().isFetchingWorkspacesFromRBAC,
+      isFetchingWorkspacesFromRBACError: store.getStore().isFetchingWorkspacesFromRBACError,
+      selectedWorkspace: store.getStore().selectedWorkspace,
+      fetchedWorkspaces: store.getStore().fetchedWorkspaces,
+      workspaceTree: store.getStore().workspaceTree,
+    }),
+    {
+      isWorkspacesMenuExpanded: store.getStore().isWorkspacesMenuExpanded,
+      isFetchingWorkspacesFromRBAC: store.getStore().isFetchingWorkspacesFromRBAC,
+      isFetchingWorkspacesFromRBACError: store.getStore().isFetchingWorkspacesFromRBACError,
+      selectedWorkspace: store.getStore().selectedWorkspace,
+      fetchedWorkspaces: store.getStore().fetchedWorkspaces,
+      workspaceTree: store.getStore().workspaceTree,
+    }
+  );
+
+  useEffect(() => {
+    const unsubscribe = store.subscribe(dispatch);
+    return unsubscribe;
+  }, [store]);
+
+  const currentStore = store.getStore();
+
+  return {
+    store: currentStore,
+    isWorkspacesMenuExpanded,
+    isFetchingWorkspacesFromRBAC,
+    isFetchingWorkspacesFromRBACError,
+    selectedWorkspace,
+    fetchedWorkspaces,
+    workspaceTree,
+    setIsWorkspacesMenuExpanded: store.setIsWorkspacesMenuExpanded,
+    setIsFetchingWorkspacesFromRBAC: store.setIsFetchingWorkspacesFromRBAC,
+    setIsFetchingWorkspacesFromRBACError: store.setIsFetchingWorkspacesFromRBACError,
+    setSelectedWorkspace: store.setSelectedWorkspace,
+    setFetchedWorkspaces: store.setFetchedWorkspaces,
+    setWorkspaceTree: store.setWorkspaceTree,
+  };
+};
+
+export { WorkspacesStoreProvider, useWorkspacesStore };
