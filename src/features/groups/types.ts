f715a0090bfb7e7942fb9a24e8326675181f6990
Riccardo Forina <rforina@redhat.com>
2025-08-04 14:14:13 +0200

    chore: restructure and test groups
    this also improves some shared utilities around modals to drop an unnecessary role attribute,
    and simplifies the code already committed that was using react-data-view to be easier to read

---
+++ src/features/groups/types.ts
@@ -0,0 +1,68 @@
+// TypeScript interfaces for Group UI components
+// Core data interfaces (Group, GroupState, Member, GroupReducerState) are now in src/redux/groups/reducer.ts
+
+// Import and re-export core interfaces from Redux for convenience
+import type { Group, GroupState, Member, GroupReducerState } from '../../redux/groups/reducer';
+export type { Group, GroupState, Member, GroupReducerState };
+
+export interface RBACStore {
+  groupReducer: GroupReducerState;
+  // Add other reducers as needed
+  [key: string]: any;
+}
+
+export interface TabItem {
+  eventKey: number;
+  title: string;
+  name: string;
+  to: string;
+}
+
+export interface OutletContext {
+  [key: string]:
+    | {
+        onDefaultGroupChanged?: (show: boolean) => void;
+      }
+    | string
+    | undefined;
+  groupId: string;
+  systemGroupUuid?: string;
+}
+
+// Additional interfaces for compatibility
+export interface ExpandedCells {
+  [key: string]: string;
+}
+
+export interface SortByState {
+  index: number;
+  direction: 'asc' | 'desc';
+}
+
+export interface EmptyGroupsStateProps {
+  hasActiveFilters: boolean;
+}
+
+export interface GroupActionsMenuProps {
+  selectedRows: Group[];
+  onCreateGroup: () => void;
+  onEditGroup: (groupId: string) => void;
+  onDeleteGroups: (groupIds: string[]) => void;
+  onSelect: () => void;
+}
+
+export interface GroupsTableProps {
+  groups: Group[];
+  expandedCells: ExpandedCells;
+  setExpandedCells: (cells: ExpandedCells) => void;
+  selectedRows: Group[];
+  onRowSelection: (selectedRows: Group[]) => void;
+  sortByState: SortByState;
+  onSort: (event: any, index: number, direction: 'asc' | 'desc') => void;
+  isAdmin: boolean;
+  isLoading?: boolean;
+  hasActiveFilters: boolean;
+  onExpansion: (groupId: string, columnKey: string, isExpanding: boolean) => void;
+  onEditGroup: (groupId: string) => void;
+  onDeleteGroups: (groupIds: string[]) => void;
+}
